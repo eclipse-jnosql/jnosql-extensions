e422160c177bf40baccdfcb2544e0010a302c47e
Otavio Santana <otaviopolianasantana@gmail.com>
2025-01-05 06:04:22 +0000

    docs: update arangodb documentation
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-tinkerpop-connections/src/main/java/org/eclipse/jnosql/mapping/tinkerpop/connections/ArangoDBGraphConfigurations.java
+++ jnosql-tinkerpop-connections/src/main/java/org/eclipse/jnosql/mapping/tinkerpop/connections/ArangoDBGraphConfigurations.java
@@ -12,42 +12,75 @@ package org.eclipse.jnosql.mapping.tinkerpop.connections;
 import java.util.function.Supplier;
 
 /**
- * An enumeration to show the available options to connect to the ArangoDB database.
- * It implements {@link Supplier}, where its it returns the property name that might be
- * overwritten by the system environment using Eclipse Microprofile or Jakarta Config API.
+ * An enumeration to represent the available configuration options for connecting to an ArangoDB database
+ * when using it as a graph database. Each enum constant corresponds to a specific property key that can
+ * be used to configure the connection and graph settings.
+ * <p>
+ * This enumeration implements {@link Supplier}, allowing each constant to provide its property key
+ * as a string value. These keys can be overridden by the system environment or a configuration source
+ * compliant with the Eclipse MicroProfile Config or Jakarta Config API.
+ * </p>
+ * <p>
+ * Example usage:
+ * </p>
+ * <pre>
+ * {@code
+ * String edgeCollectionKey = ArangoDBGraphConfigurations.EDGE.get();
+ * String graphName = ArangoDBGraphConfigurations.GRAPH.get();
+ * String host = ArangoDBGraphConfigurations.HOST.get();
+ * }
+ * </pre>
+ * <p>
+ * Default property keys and their descriptions:
+ * <ul>
+ *   <li><b>EDGE</b>: Specifies the edge collection prefix. Example: {@code jnosql.arangodb.graph.edge.1=edge}</li>
+ *   <li><b>EDGE_RELATIONSHIP</b>: Specifies the edge collection, source vertex collection, and target vertex collection, separated by a pipe.
+ *   Example: {@code jnosql.arangodb.graph.relationship.1=Person|knows|Person}</li>
+ *   <li><b>VERTEX</b>: Specifies the vertex collection prefix. Example: {@code jnosql.arangodb.graph.vertex.1=vertex}</li>
+ *   <li><b>GRAPH</b>: Specifies the name of the graph to use.</li>
+ *   <li><b>HOST</b>: Specifies the database host.</li>
+ *   <li><b>USER</b>: Specifies the username credential for the database connection.</li>
+ *   <li><b>PASSWORD</b>: Specifies the password credential for the database connection.</li>
+ * </ul>
+ * </p>
  *
  * @see org.eclipse.jnosql.communication.Settings
  */
 public enum ArangoDBGraphConfigurations implements Supplier<String> {
 
     /**
-     * The edge collection. It uses as a prefix. E.g.:jnosql.arangodb.graph.edge.1=edge
+     * The edge collection prefix. Example: {@code jnosql.arangodb.graph.edge.1=edge}.
      */
     EDGE("jnosql.arangodb.graph.edge"),
+
     /**
-     * Edge collection, the source vertex collection and the target vertex collection split by pipe.
-     * It hou,It uses as a prefix.
-     * E.g.: jnosql.arangodb.graph.relationship.1=Person|knows|Person
+     * Specifies the edge collection, source vertex collection, and target vertex collection, separated by a pipe.
+     * Example: {@code jnosql.arangodb.graph.relationship.1=Person|knows|Person}.
      */
     EDGE_RELATIONSHIP("jnosql.arangodb.graph.relationship"),
+
     /**
-     * The vertex collection. It uses as a prefix. E.g.: jnosql.arangodb.graph.vertex.1=vertex
+     * The vertex collection prefix. Example: {@code jnosql.arangodb.graph.vertex.1=vertex}.
      */
     VERTEX("jnosql.arangodb.graph.vertex"),
+
     /**
-     * Name of the graph to use.
+     * The name of the graph to use in the ArangoDB database.
      */
     GRAPH("jnosql.arangodb.graph.graph"),
+
     /**
-     * The database host.
+     * The database host for the ArangoDB connection.
      */
     HOST("jnosql.arangodb.graph.host"),
+
     /**
-     * The user's credential.
+     * The username credential for accessing the ArangoDB database.
      */
     USER("jnosql.arangodb.graph.user"),
+
     /**
-     * The password's credential.
+     * The password credential for accessing the ArangoDB database.
      */
     PASSWORD("jnosql.arangodb.graph.password");
 
@@ -62,3 +95,4 @@ public enum ArangoDBGraphConfigurations implements Supplier<String> {
         return value;
     }
 }
+
