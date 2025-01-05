050c00a16f897b171ab43914762bef7333849e33
Otavio Santana <otaviopolianasantana@gmail.com>
2025-01-05 07:35:45 +0000

    docs: update documentation on javadoc at package info
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-tinkerpop-connections/src/main/java/org/eclipse/jnosql/mapping/tinkerpop/connections/package-info.java
+++ jnosql-tinkerpop-connections/src/main/java/org/eclipse/jnosql/mapping/tinkerpop/connections/package-info.java
@@ -13,5 +13,57 @@
  *   Otavio Santana
  */
 
-
+/**
+ * This package contains classes and configurations for integrating with graph databases
+ * using the Apache TinkerPop framework, with support for various database backends such as Neo4J,
+ * JanusGraph, ArangoDB, and Titan.
+ * <p>
+ * The primary purpose of this package is to provide implementations of the {@link org.apache.tinkerpop.gremlin.structure.Graph}
+ * interface through database-specific configurations, enabling seamless connection and interaction with graph databases.
+ * </p>
+ * <p>
+ * Key components in this package include:
+ * <ul>
+ *   <li>{@link org.eclipse.jnosql.mapping.tinkerpop.connections.Neo4JGraphConfiguration}:
+ *       Configures and connects to Neo4J in remote mode.</li>
+ *   <li>{@link org.eclipse.jnosql.mapping.tinkerpop.connections.Neo4JEmbeddedGraphConfiguration}:
+ *       Configures and connects to Neo4J in embedded mode.</li>
+ *   <li>{@link org.eclipse.jnosql.mapping.tinkerpop.connections.JanusGraphConfiguration}:
+ *       Configures and connects to a JanusGraph instance.</li>
+ *   <li>{@link org.eclipse.jnosql.mapping.tinkerpop.connections.TitanGraphConfiguration}:
+ *       Configures and connects to a Titan graph database.</li>
+ *   <li>{@link org.eclipse.jnosql.mapping.tinkerpop.connections.ArangoDBGraphConfiguration}:
+ *       Configures and connects to an ArangoDB graph database.</li>
+ * </ul>
+ * </p>
+ * <p>
+ * The package also includes enumeration classes, such as:
+ * <ul>
+ *   <li>{@link org.eclipse.jnosql.mapping.tinkerpop.connections.ArangoDBGraphConfigurations}:
+ *       Provides configuration keys for ArangoDB-specific settings.</li>
+ *   <li>{@link org.eclipse.jnosql.mapping.tinkerpop.connections.Neo4JGraphConfigurations}:
+ *       Provides configuration keys for Neo4J-specific settings.</li>
+ * </ul>
+ * </p>
+ * <p>
+ * Example usage of a graph configuration:
+ * </p>
+ * <pre>
+ * {@code
+ * Settings settings = new Settings();
+ * settings.put(ArangoDBGraphConfigurations.HOST.get() + ".1", "localhost:8529");
+ * settings.put(ArangoDBGraphConfigurations.USER.get(), "root");
+ * settings.put(ArangoDBGraphConfigurations.PASSWORD.get(), "password");
+ * settings.put(ArangoDBGraphConfigurations.GRAPH.get(), "my_graph");
+ *
+ * GraphConfiguration configuration = new ArangoDBGraphConfiguration();
+ * Graph graph = configuration.apply(settings);
+ * }
+ * </pre>
+ * <p>
+ * All configurations implement {@link org.eclipse.jnosql.communication.Settings} for dynamic
+ * property management and leverage Eclipse MicroProfile Config or Jakarta Config APIs for
+ * externalized configuration.
+ * </p>
+ */
 package org.eclipse.jnosql.mapping.tinkerpop.connections;
