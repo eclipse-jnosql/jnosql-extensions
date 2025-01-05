7b5baa50f9053f3c8021342de23f0db0a49de60a
Otavio Santana <otaviopolianasantana@gmail.com>
2025-01-05 05:28:05 +0000

    feat: update apache tinkerpop
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-tinkerpop-connections/src/main/java/org/eclipse/jnosql/mapping/tinkerpop/connections/package-info.java
+++ jnosql-tinkerpop-connections/src/main/java/org/eclipse/jnosql/mapping/tinkerpop/connections/package-info.java
@@ -13,14 +13,5 @@
  *   Otavio Santana
  */
 
-/**
- * Apache Cassandra is a free and open-source distributed database management system designed to handle large amounts of
- * data across many commodity servers, providing high availability with no single point of failure. Cassandra offers
- * robust support for clusters spanning multiple datacenters, with asynchronous masterless replication allowing low
- * latency operations for all clients.
- * Cassandra also places a high value on performance. In 2012,
- * University of Toronto researchers studying NoSQL systems concluded that "In terms of scalability, there is a clear
- * winner throughout our experiments. Cassandra achieves the highest throughput for the maximum number
- * of nodes in all experiments" although "this comes at the price of high write and read latencies.
- */
+
 package org.eclipse.jnosql.mapping.tinkerpop.connections;
