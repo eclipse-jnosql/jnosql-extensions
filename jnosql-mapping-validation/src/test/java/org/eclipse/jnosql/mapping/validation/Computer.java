22361513307c609c30dbf449e9178ab564451863
Otavio Santana <otaviopolianasantana@gmail.com>
2025-01-05 16:43:29 +0000

    feat: udpate compputer at validation to record
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-mapping-validation/src/test/java/org/eclipse/jnosql/mapping/validation/Computer.java
+++ jnosql-mapping-validation/src/test/java/org/eclipse/jnosql/mapping/validation/Computer.java
@@ -18,30 +18,12 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-public class Computer {
-
-    private final String name;
-
-    private final int version;
-
-    private final String model;
+public record Computer(String name, int version, String model) {
 
     public Computer(@NotNull String name, @Min(2020) int version,
                     @NotNull @Size(min = 2, max = 4) String model) {
         this.name = name;
         this.version = version;
         this.model = model;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getVersion() {
-        return version;
-    }
-
-    public String getModel() {
-        return model;
     }
 }
