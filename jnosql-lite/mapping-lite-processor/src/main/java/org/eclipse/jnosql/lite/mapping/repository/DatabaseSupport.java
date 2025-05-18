f707d3ce59eb7111b4a84b5f965cb65375cb61e7
Otavio Santana <otaviopolianasantana@gmail.com>
2025-05-18 17:37:22 +0100

    feat: refactor the code for database support using more solid principle
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-lite/mapping-lite-processor/src/main/java/org/eclipse/jnosql/lite/mapping/repository/DatabaseSupport.java
+++ jnosql-lite/mapping-lite-processor/src/main/java/org/eclipse/jnosql/lite/mapping/repository/DatabaseSupport.java
@@ -9,34 +9,35 @@ import java.util.stream.Collectors;
 
 enum DatabaseSupport {
 
-    DOCUMENT("org.eclipse.jnosql.mapping.document.DocumentTemplate"),
-    COLUMN("org.eclipse.jnosql.mapping.column.ColumnTemplate"),
-    KEY_VALUE("org.eclipse.jnosql.mapping.keyvalue.KeyValueTemplate"),
-    GRAPH("org.eclipse.jnosql.mapping.graph.GraphTemplate");
+    DOCUMENT("org.eclipse.jnosql.mapping.document.DocumentTemplate", DatabaseType.DOCUMENT),
+    COLUMN("org.eclipse.jnosql.mapping.column.ColumnTemplate", DatabaseType.COLUMN),
+    KEY_VALUE("org.eclipse.jnosql.mapping.keyvalue.KeyValueTemplate", DatabaseType.KEY_VALUE),
+    GRAPH("org.eclipse.jnosql.mapping.graph.GraphTemplate", DatabaseType.GRAPH);
 
     private final String requiredClassName;
+    private final DatabaseType databaseType;
 
-    DatabaseSupport(String requiredClassName) {
+    DatabaseSupport(String requiredClassName, DatabaseType databaseType) {
         this.requiredClassName = requiredClassName;
+        this.databaseType = databaseType;
     }
 
-    public boolean isAvailable() {
-        return isClassPresent(requiredClassName);
-    }
-
-    private static boolean isClassPresent(String className) {
+    boolean isSupported() {
         try {
-            Class.forName(className);
+            Class.forName(requiredClassName);
             return true;
         } catch (ClassNotFoundException e) {
             return false;
         }
     }
 
+    DatabaseType getDatabaseType() {
+        return databaseType;
+    }
 
     static Set<DatabaseType> detectSupportedTypes() {
         return Arrays.stream(values())
-                .filter(DatabaseSupport::isAvailable)
+                .filter(DatabaseSupport::isSupported)
                 .map(DatabaseSupport::getDatabaseType)
                 .collect(Collectors.toCollection(() -> EnumSet.noneOf(DatabaseType.class)));
     }
