bfa91485b6d4689cd433da175cf2f3a71e092f1b
Otavio Santana <otaviopolianasantana@gmail.com>
2025-06-03 08:36:46 +0100

    feat: remove colun annotation
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-static-metamodel/jnosql-metamodel-sample/src/main/java/org/eclipse/jnsoql/entities/WishList.java
+++ jnosql-static-metamodel/jnosql-metamodel-sample/src/main/java/org/eclipse/jnsoql/entities/WishList.java
@@ -14,7 +14,6 @@
  */
 package org.eclipse.jnsoql.entities;
 
-import jakarta.nosql.Column;
 import jakarta.nosql.Entity;
 import jakarta.nosql.Id;
 
@@ -28,13 +27,10 @@ public class WishList {
     @Id
     private UUID id;
 
-    @Column
     private List<String> products;
 
-    @Column
     private String[] travels;
 
-    @Column
     private Map<String, String> people;
 
 }
