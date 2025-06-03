2f57b89aef8399c6ed99e4d66c380269ba52d8d9
Otavio Santana <otaviopolianasantana@gmail.com>
2025-06-03 19:20:54 +0100

    test: return wishlist with element
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-static-metamodel/jnosql-metamodel-sample/src/main/java/org/eclipse/jnsoql/entities/WishList.java
+++ jnosql-static-metamodel/jnosql-metamodel-sample/src/main/java/org/eclipse/jnsoql/entities/WishList.java
@@ -14,6 +14,7 @@
  */
 package org.eclipse.jnsoql.entities;
 
+import jakarta.nosql.Column;
 import jakarta.nosql.Entity;
 import jakarta.nosql.Id;
 
@@ -27,10 +28,13 @@ public class WishList {
     @Id
     private UUID id;
 
+    @Column
     private List<String> products;
 
+    @Column
     private String[] travels;
 
+    @Column
     private Map<String, String> people;
 
 }
