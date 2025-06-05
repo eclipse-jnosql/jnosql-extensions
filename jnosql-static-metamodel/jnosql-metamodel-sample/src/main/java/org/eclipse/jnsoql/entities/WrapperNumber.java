5d3308ac3329fa41de44e14dec19013d1fa5c5a2
Otavio Santana <otaviopolianasantana@gmail.com>
2025-06-05 08:43:48 +0100

    test: create wrapper class
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ jnosql-static-metamodel/jnosql-metamodel-sample/src/main/java/org/eclipse/jnsoql/entities/WrapperNumber.java
@@ -0,0 +1,45 @@
+/*
+ *  Copyright (c) 2025 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnsoql.entities;
+
+import jakarta.nosql.Column;
+import jakarta.nosql.Entity;
+import jakarta.nosql.Id;
+
+import java.util.UUID;
+
+@Entity
+public class WrapperNumber {
+
+    @Id
+    private UUID uuid;
+    @Column
+    private int integer;
+    @Column
+    private long longNumber;
+    @Column
+    private float floatNumber;
+    @Column
+    private double doubleNumber;
+    @Column
+    private byte byteNumber;
+
+    @Column
+    private short shortNumber;
+
+    @Column
+    private boolean booleanSample;
+
+}
