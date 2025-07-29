0d1dc7f803701cad9712c91b7e3bb59c35ecee4a
Otavio Santana <otaviopolianasantana@gmail.com>
2025-07-29 08:42:04 +0100

    feat: update the version 1.1.
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-lite/mapping-lite-document-test/src/test/java/org/eclipse/jnosql/lite/mapping/entities/PersonRepositoryTest.java
+++ jnosql-lite/mapping-lite-document-test/src/test/java/org/eclipse/jnosql/lite/mapping/entities/PersonRepositoryTest.java
@@ -241,12 +241,22 @@ class PersonRepositoryTest {
 
     @Test
     void shouldQuery(){
-        when(template.prepare(anyString())).thenReturn(Mockito.mock(PreparedStatement.class));
+        when(template.prepare(anyString(), Mockito.eq("Person"))).thenReturn(Mockito.mock(PreparedStatement.class));
         this.personRepository.query("Ada");
         ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
-        verify(template).prepare(captor.capture());
+        verify(template).prepare(captor.capture(), Mockito.eq("Person"));
         String value = captor.getValue();
-        assertThat(value).isEqualTo("select * from Person where name = @name");
+        assertThat(value).isEqualTo("from Person where name = :name");
+    }
+
+    @Test
+    void shouldQuery2() {
+        when(template.prepare(anyString(), Mockito.eq("Person"))).thenReturn(Mockito.mock(PreparedStatement.class));
+        this.personRepository.query2("Ada");
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        verify(template).prepare(captor.capture(), Mockito.eq("Person"));
+        String value = captor.getValue();
+        assertThat(value).isEqualTo("where name = :name");
     }
 
     @Test
