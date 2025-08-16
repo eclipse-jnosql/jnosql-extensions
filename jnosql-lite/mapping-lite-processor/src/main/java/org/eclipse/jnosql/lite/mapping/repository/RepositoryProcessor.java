e633acae61deac408464934e9e4387cc8ba1c27d
Ondro Mihalyi <mihalyi@omnifish.ee>
2025-08-16 03:14:49 +0200

    chore: Jakarta Persistence Driver - headers

--- jnosql-lite/mapping-lite-processor/src/main/java/org/eclipse/jnosql/lite/mapping/repository/RepositoryProcessor.java
+++ jnosql-lite/mapping-lite-processor/src/main/java/org/eclipse/jnosql/lite/mapping/repository/RepositoryProcessor.java
@@ -1,4 +1,5 @@
 /*
+ *  Copyright (c) 2025 Contributors to the Eclipse Foundation
  *  Copyright (c) 2021 Otávio Santana and others
  *   All rights reserved. This program and the accompanying materials
  *   are made available under the terms of the Eclipse Public License v1.0
