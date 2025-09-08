2376f7424d1677c26d0e3a6474ed8eb67e326546
Ondro Mihalyi <mihalyi@omnifish.ee>
2025-09-08 09:40:58 +0200

    chore: Jakarta Persistence Driver - moved tests in scanner to a different package
    The tests were moved from the driver project, they should be in a different package which the driver project doesn't use

---
+++ jnosql-jakarta-persistence/jnosql-jakarta-persistence-scanner/src/test/java/ee/omnifish/jnosql/jakartapersistence/scanner/CountCalls.java
@@ -0,0 +1,29 @@
+/*
+ *  Copyright (c) 2025 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Ondro Mihalyi
+ */
+package ee.omnifish.jnosql.jakartapersistence.scanner;
+
+import jakarta.interceptor.InterceptorBinding;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@InterceptorBinding
+@Target({TYPE, METHOD})
+@Retention(RUNTIME)
+public @interface CountCalls {
+}
---
+++ jnosql-jakarta-persistence/jnosql-jakarta-persistence-scanner/src/test/java/ee/omnifish/jnosql/jakartapersistence/scanner/QualifierTest.java
@@ -0,0 +1,69 @@
+/*
+ *  Copyright (c) 2025 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Ondro Mihalyi
+ */
+package ee.omnifish.jnosql.jakartapersistence.scanner;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import jakarta.enterprise.inject.se.SeContainer;
+import jakarta.enterprise.inject.se.SeContainerInitializer;
+import jakarta.enterprise.inject.spi.CDI;
+
+import org.eclipse.jnosql.jakartapersistence.communication.PersistenceDatabaseManager;
+import org.eclipse.jnosql.jakartapersistence.mapping.PersistenceDocumentTemplate;
+import org.eclipse.jnosql.jakartapersistence.mapping.spi.JakartaPersistenceExtension;
+import org.eclipse.jnosql.mapping.core.Converters;
+import org.eclipse.jnosql.mapping.reflection.Reflections;
+import org.eclipse.jnosql.mapping.reflection.spi.ReflectionEntityMetadataExtension;
+import org.eclipse.jnosql.mapping.semistructured.EntityConverter;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class QualifierTest {
+
+    private SeContainer cdiContainer;
+
+    @BeforeEach
+    void init() {
+        cdiContainer = SeContainerInitializer.newInstance()
+                .disableDiscovery()
+                .addPackages(Converters.class, EntityConverter.class)
+                .addPackages(Reflections.class)
+                .addExtensions(ReflectionEntityMetadataExtension.class, JakartaPersistenceExtension.class)
+                .addPackages(PersistenceDocumentTemplate.class, PersistenceDatabaseManager.class)
+                .addBeanClasses(EntityManagerProducer.class)
+                .initialize();
+    }
+
+    @AfterEach
+    void cleanup() {
+        cdiContainer.close();
+    }
+
+    @Test
+    void repositoryCanBeInjectedWithQualifier() {
+        var repository = CDI.current().select(QualifiedPersonRepository.class, SpecialRepository.Literal.INSTANCE).get();
+        assertThat(repository, notNullValue());
+    }
+
+    @Test
+    void repositoryCannotBeInjectedWithoutQualifier() {
+        assertThrows(Exception.class, () -> {
+            CDI.current().select(QualifiedPersonRepository.class).get();
+        });
+    }
+}
