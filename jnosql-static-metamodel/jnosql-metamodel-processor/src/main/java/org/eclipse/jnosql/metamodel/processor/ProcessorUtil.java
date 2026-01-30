52ba4fa0c4ae5e64876c451e87a71f55ccbdba03
Otavio Santana <otaviopolianasantana@gmail.com>
2026-01-30 21:18:22 +0000

    refactor: remove unused regex pattern from ProcessorUtil

--- jnosql-static-metamodel/jnosql-metamodel-processor/src/main/java/org/eclipse/jnosql/metamodel/processor/ProcessorUtil.java
+++ jnosql-static-metamodel/jnosql-metamodel-processor/src/main/java/org/eclipse/jnosql/metamodel/processor/ProcessorUtil.java
@@ -18,22 +18,12 @@ import javax.lang.model.element.Element;
 import javax.lang.model.element.PackageElement;
 import javax.lang.model.element.TypeElement;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import static java.util.Locale.ENGLISH;
-
 /**
  * Utility class containing static methods for processing and manipulating elements in annotation processors.
  * This class provides methods for retrieving package names, simple names, capitalizing strings, checking element types,
  * and extracting data from type strings.
  */
 public final class ProcessorUtil {
-
-    /**
-     * A compiled regular expression pattern for extracting text enclosed in angle brackets.
-     */
-    public static final Pattern COMPILE = Pattern.compile("<(.*?)>");
 
     private ProcessorUtil() {
     }
