19e469431077ba9c6595f8fc5feb2b66947f61d5
Otavio Santana <otaviopolianasantana@gmail.com>
2026-01-31 21:42:32 +0000

    refactor: remove unused import for URISyntaxException in RepositoryMetadataProcessor

--- jnosql-lite/mapping-lite-processor/src/main/java/org/eclipse/jnosql/lite/mapping/RepositoryMetadataProcessor.java
+++ jnosql-lite/mapping-lite-processor/src/main/java/org/eclipse/jnosql/lite/mapping/RepositoryMetadataProcessor.java
@@ -29,7 +29,6 @@ import javax.tools.Diagnostic;
 import javax.tools.JavaFileObject;
 import java.io.IOException;
 import java.io.Writer;
-import java.net.URISyntaxException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
