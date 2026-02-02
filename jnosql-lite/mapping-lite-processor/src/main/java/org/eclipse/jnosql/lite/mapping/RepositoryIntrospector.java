5bfc7ad309d88cbdc0a2b5a416cea830da671a68
Otavio Santana <otaviopolianasantana@gmail.com>
2026-02-02 18:21:03 +0000

    feat: enhance RepositoryIntrospector to generate mapping interfaces using Mustache templates

--- jnosql-lite/mapping-lite-processor/src/main/java/org/eclipse/jnosql/lite/mapping/RepositoryIntrospector.java
+++ jnosql-lite/mapping-lite-processor/src/main/java/org/eclipse/jnosql/lite/mapping/RepositoryIntrospector.java
@@ -14,17 +14,28 @@
  */
 package org.eclipse.jnosql.lite.mapping;
 
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.Mustache;
+import com.github.mustachejava.MustacheFactory;
+
+import javax.annotation.processing.Filer;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.DeclaredType;
 import javax.lang.model.type.TypeKind;
+import javax.tools.Diagnostic;
+import javax.tools.JavaFileObject;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.Collections;
 import java.util.Set;
 import java.util.function.Supplier;
 import java.util.logging.Logger;
 
 public class RepositoryIntrospector implements Supplier<MappingResult> {
+
+    private static final String NEW_INSTANCE = "repository_metadata.mustache";
 
     private static final Set<String> JAKARTA_DATA_REPOSITORIES = Set.of(
             "jakarta.data.repository.DataRepository",
@@ -39,24 +50,38 @@ public class RepositoryIntrospector implements Supplier<MappingResult> {
     private final ProcessingEnvironment processingEnv;
     private final EntityMappingIntrospector entityMappingIntrospector;
     private final ProjectionMappingIntrospector projectionMappingIntrospector;
+    private final Mustache template;
 
     RepositoryIntrospector(Element element, ProcessingEnvironment processingEnv) {
         this.element = element;
         this.processingEnv = processingEnv;
         this.entityMappingIntrospector = new EntityMappingIntrospector(element, processingEnv);
         this.projectionMappingIntrospector = new ProjectionMappingIntrospector(element, processingEnv);
+        MustacheFactory factory = new DefaultMustacheFactory();
+        this.template = factory.compile(NEW_INSTANCE);
+
     }
 
     @Override
     public MappingResult get() {
         if(element instanceof TypeElement repository){
-            LOGGER.info("Processing the repository: " + repository);
-            String packageName = ProcessorUtil.getPackageName(repository);
-            String entity = entityOptionalLiteral(repository);
-            String type = ProcessorUtil.getSimpleNameAsString(repository);
-            var metaData = new RepositoryMetaModel(packageName, entity, type, Collections.emptyList());
+            return generateMappingInterface(repository);
         }
         return new MappingResult(MappingCategory.PROJECTION, "");
+    }
+
+    private MappingResult generateMappingInterface(TypeElement repository) {
+        LOGGER.info("Processing the repository: " + repository);
+        String packageName = ProcessorUtil.getPackageName(repository);
+        String entity = entityOptionalLiteral(repository);
+        String type = ProcessorUtil.getSimpleNameAsString(repository);
+        var metadata = new RepositoryMetaModel(packageName, entity, type, Collections.emptyList());
+        try {
+            createClass(element, metadata);
+        } catch (IOException exception) {
+            error(exception);
+        }
+        return new MappingResult(MappingCategory.ENTITY, metadata.getQualified());
     }
 
     private String entityOptionalLiteral(TypeElement repository) {
@@ -78,5 +103,18 @@ public class RepositoryIntrospector implements Supplier<MappingResult> {
                 .map(te -> "Optional.of(" + te.getQualifiedName() + ".class)")
                 .findFirst()
                 .orElse("Optional.empty()");
+    }
+
+    private void createClass(Element entity, RepositoryMetaModel metadata) throws IOException {
+        Filer filer = processingEnv.getFiler();
+        JavaFileObject fileObject = filer.createSourceFile(metadata.getQualified(), entity);
+        try (Writer writer = fileObject.openWriter()) {
+            template.execute(writer, metadata);
+        }
+    }
+
+    private void error(IOException exception) {
+        processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "failed to write extension file: "
+                + exception.getMessage());
     }
 }
