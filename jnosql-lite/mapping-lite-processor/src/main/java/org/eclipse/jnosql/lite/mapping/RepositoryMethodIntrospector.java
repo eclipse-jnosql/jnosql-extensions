5598ad6849cbccadedb191671d90b9a02de469f2
Otavio Santana <otaviopolianasantana@gmail.com>
2026-02-04 20:00:27 +0000

    chore: initial structure to call the method on RepositoryIntrospector

--- jnosql-lite/mapping-lite-processor/src/main/java/org/eclipse/jnosql/lite/mapping/RepositoryMethodIntrospector.java
+++ jnosql-lite/mapping-lite-processor/src/main/java/org/eclipse/jnosql/lite/mapping/RepositoryMethodIntrospector.java
@@ -14,22 +14,54 @@
  */
 package org.eclipse.jnosql.lite.mapping;
 
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.Mustache;
+import com.github.mustachejava.MustacheFactory;
+
+import javax.annotation.processing.Filer;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.lang.model.element.Element;
+import javax.tools.Diagnostic;
+import javax.tools.JavaFileObject;
+import java.io.IOException;
+import java.io.Writer;
 
 final class RepositoryMethodIntrospector {
+
+    private static final String MUSTACHE_TEMPLATE = "repository_metadata.mustache";
 
     private final Element method;
     private final String type;
     private final ProcessingEnvironment processingEnv;
+    private final Mustache template;
 
     RepositoryMethodIntrospector(Element method, String type, ProcessingEnvironment processingEnv) {
         this.method = method;
         this.type = type;
         this.processingEnv = processingEnv;
+        MustacheFactory factory = new DefaultMustacheFactory();
+        this.template = factory.compile(MUSTACHE_TEMPLATE);
     }
 
     public static RepositoryMethodIntrospector of(Element method, String type, ProcessingEnvironment processingEnv) {
         return new RepositoryMethodIntrospector(method, type, processingEnv);
+    }
+
+    String generateMethodClass() {
+        return "";
+    }
+
+
+    private void createClass(Element entity, RepositoryMetaModel metadata) throws IOException {
+        Filer filer = processingEnv.getFiler();
+        JavaFileObject fileObject = filer.createSourceFile(metadata.getQualified(), entity);
+        try (Writer writer = fileObject.openWriter()) {
+            template.execute(writer, metadata);
+        }
+    }
+
+    private void error(IOException exception) {
+        processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "failed to write extension file: "
+                + exception.getMessage());
     }
 }
