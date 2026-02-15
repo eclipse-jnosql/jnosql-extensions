682aa5ae0ea2f60ce624a58af390789215c8b403
Otavio Santana <otaviopolianasantana@gmail.com>
2026-02-15 07:35:45 +0000

    chore: update template to by

--- jnosql-lite/mapping-lite-processor/src/main/java/org/eclipse/jnosql/lite/mapping/RepositoryMethodParameterIntrospector.java
+++ jnosql-lite/mapping-lite-processor/src/main/java/org/eclipse/jnosql/lite/mapping/RepositoryMethodParameterIntrospector.java
@@ -72,7 +72,8 @@ final class RepositoryMethodParameterIntrospector {
         String constraint = Optional.ofNullable(variableElement.getAnnotation(Is.class))
                 .map(Object::toString)
                 .map(s -> s.substring(s.indexOf(".Is(")+4, s.lastIndexOf(")")))
-                .orElse("null");
+                .map("Optional.of(%s)"::formatted)
+                .orElse("Optional.empty()");
         var name = variableElement.getSimpleName().toString();
         var param = Optional.ofNullable(variableElement.getAnnotation(Param.class))
                 .map(Param::value).orElse(name);
