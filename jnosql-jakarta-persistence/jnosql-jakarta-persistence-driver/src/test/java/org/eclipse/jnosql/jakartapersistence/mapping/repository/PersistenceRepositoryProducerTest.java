3b970365a955f766b17523a303e485059c5af2b1
Otavio Santana <otaviopolianasantana@gmail.com>
2026-03-12 18:19:47 +0000

    test: add setup and teardown for CDI container in PersistenceRepositoryProducerTest

--- jnosql-jakarta-persistence/jnosql-jakarta-persistence-driver/src/test/java/org/eclipse/jnosql/jakartapersistence/mapping/repository/PersistenceRepositoryProducerTest.java
+++ jnosql-jakarta-persistence/jnosql-jakarta-persistence-driver/src/test/java/org/eclipse/jnosql/jakartapersistence/mapping/repository/PersistenceRepositoryProducerTest.java
@@ -14,8 +14,46 @@
  */
 package org.eclipse.jnosql.jakartapersistence.mapping.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import ee.omnifish.jnosql.jakartapersistence.Person;
+import ee.omnifish.jnosql.jakartapersistence.TestJakartaPersistenceClassScanner;
+import ee.omnifish.jnosql.jakartapersistence.TestSupport;
+import jakarta.enterprise.inject.se.SeContainer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Set;
+
 
 class PersistenceRepositoryProducerTest {
 
+    private SeContainer cdiContainer;
+
+    private ComputerRepository computerRepository;
+
+    private Computers computers;
+
+    @BeforeEach
+    void init() {
+        TestJakartaPersistenceClassScanner.customRepositories = Set.of(Computers.class);
+        TestJakartaPersistenceClassScanner.standardRepositories = Set.of(ComputerRepository.class);
+        cdiContainer = RepositorySupport.cdiInitializerWithDefaultEmProducer()
+                .initialize();
+        this.computerRepository = cdiContainer.select(ComputerRepository.class).get();
+        this.computers = cdiContainer.select(Computers.class).get();
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (cdiContainer != null) {
+            cdiContainer.close();
+        }
+    }
+
+    @Test
+    void findAll() {
+        var computers = computerRepository.findAll().toList();
+
+    }
 }
