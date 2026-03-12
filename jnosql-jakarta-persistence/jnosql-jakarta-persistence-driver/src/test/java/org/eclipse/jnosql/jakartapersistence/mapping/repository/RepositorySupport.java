d9ee0b0cda62273ea295c66042f969567e00054a
Otavio Santana <otaviopolianasantana@gmail.com>
2026-03-12 18:19:43 +0000

    feat: add Computers repository interface with @Repository annotation

---
+++ jnosql-jakarta-persistence/jnosql-jakarta-persistence-driver/src/test/java/org/eclipse/jnosql/jakartapersistence/mapping/repository/RepositorySupport.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2024 Contributors to the Eclipse Foundation
+ *
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  and Apache License v2.0 which accompanies this distribution.
+ *  The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *  and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *  You may elect to redistribute this code under either of these licenses.
+ *
+ *  Contributors:
+ *
+ *  Ondro Mihalyi
+ */
+package org.eclipse.jnosql.jakartapersistence.mapping.repository;
+
+import ee.omnifish.jnosql.jakartapersistence.EntityManagerProducer;
+import jakarta.enterprise.inject.se.SeContainerInitializer;
+import org.eclipse.jnosql.jakartapersistence.communication.PersistenceDatabaseManager;
+import org.eclipse.jnosql.jakartapersistence.mapping.PersistenceDocumentTemplate;
+import org.eclipse.jnosql.jakartapersistence.mapping.cache.PersistenceUnitCacheProvider;
+import org.eclipse.jnosql.jakartapersistence.mapping.spi.JakartaPersistenceExtension;
+
+/**
+ *
+ * @author Ondro Mihalyi
+ */
+public class RepositorySupport {
+
+    private RepositorySupport() {}
+
+    public static SeContainerInitializer cdiInitializerWithDefaultEmProducer() {
+        return SeContainerInitializer.newInstance()
+                .disableDiscovery()
+                .addExtensions(JakartaPersistenceExtension.class)
+                .addPackages(PersistenceDocumentTemplate.class, PersistenceDatabaseManager.class)
+                .addBeanClasses(EntityManagerProducer.class, PersistenceUnitCacheProvider.class);
+    }
+
+
+}
