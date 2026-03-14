c6afd2fd0ce5779998c3f3c47030a648c9037c73
Otavio Santana <otaviopolianasantana@gmail.com>
2026-03-14 21:22:56 +0000

    feat: implement entity resolution and typed query generation in SqlSelectQueryConverter

--- jnosql-jakarta-persistence/jnosql-jakarta-persistence-driver/src/main/java/org/eclipse/jnosql/extensions/sql/SqlSelectQueryConverter.java
+++ jnosql-jakarta-persistence/jnosql-jakarta-persistence-driver/src/main/java/org/eclipse/jnosql/extensions/sql/SqlSelectQueryConverter.java
@@ -15,5 +15,52 @@
  */
 package org.eclipse.jnosql.extensions.sql;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+import org.eclipse.jnosql.communication.semistructured.SelectQuery;
+
 class SqlSelectQueryConverter {
+
+    private final EntityManager manager;
+
+    SqlSelectQueryConverter(EntityManager manager) {
+        this.manager = manager;
+    }
+
+    <T> T getSelectTypedQuery(SelectQuery query) {
+        Class<T> type = resolveEntity(query.name());
+
+        CriteriaBuilder criteriaBuilder = manager.getCriteriaBuilder();
+        CriteriaQuery<T> criteriaQuery = criteriaBuilder.createQuery(type);
+
+        Root<T> root = criteriaQuery.from(type);
+
+        applyColumns(query, root, criteriaQuery);
+        applyCondition(query, criteriaBuilder, root, criteriaQuery);
+        applySort(query, criteriaBuilder, root, criteriaQuery);
+        TypedQuery<T> typed = manager.createQuery(criteriaQuery);
+        applyPagination(query, typed);
+        return typed;
+    }
+
+    private <T> void applyColumns(SelectQuery query, Root<T> root, CriteriaQuery<T> criteriaQuery) {
+
+    }
+
+    @SuppressWarnings("unchecked")
+    private <FROM> Class<FROM> resolveEntity(String name) {
+        return manager.getMetamodel()
+                .getEntities()
+                .stream()
+                .filter(entity ->
+                        entity.getName().equals(name) ||
+                                entity.getJavaType().getSimpleName().equals(name))
+                .map(entity -> (Class<FROM>) entity.getJavaType())
+                .findFirst()
+                .orElseThrow(() ->
+                        new IllegalArgumentException("Entity not found: " + name));
+    }
 }
