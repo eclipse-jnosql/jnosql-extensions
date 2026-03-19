83c2fdb9a7faab6deca4c356e1b3facc0d2cb58d
Otavio Santana <otaviopolianasantana@gmail.com>
2026-03-19 20:33:26 +0000

    feat: enhance package-info.java with SQL repository support details

---
+++ jnosql-jakarta-persistence/jnosql-jakarta-persistence-driver/src/main/java/org/eclipse/jnosql/extensions/sql/repository/NoSQLRepositorySqlAdapter.java
@@ -0,0 +1,6 @@
+package org.eclipse.jnosql.extensions.sql.repository;
+
+import org.eclipse.jnosql.mapping.NoSQLRepository;
+
+public class NoSQLRepositorySqlAdapter<T, K> implements NoSQLRepository<T, K> {
+}
