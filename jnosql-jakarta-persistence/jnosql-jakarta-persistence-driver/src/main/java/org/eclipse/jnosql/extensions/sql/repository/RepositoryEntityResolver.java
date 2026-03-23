5d2d298a9c47de41b2747a595dfb670ce10d1826
Otavio Santana <otaviopolianasantana@gmail.com>
2026-03-23 20:04:19 +0000

    feat: implement dynamic proxy creation for repository instances in SqlRepositoryProducer

---
+++ jnosql-jakarta-persistence/jnosql-jakarta-persistence-driver/src/main/java/org/eclipse/jnosql/extensions/sql/repository/RepositoryEntityResolver.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) 2026 Contributors to the Eclipse Foundation
+ *
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  and Apache License v2.0 which accompanies this distribution.
+ *  The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *  and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *  You may elect to redistribute this code under either of these licenses.
+ *
+ *  Contributors:
+ *
+ *  Otavio Santana
+ */
+package org.eclipse.jnosql.extensions.sql.repository;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Objects;
+import java.util.Optional;
+
+enum RepositoryEntityResolver {
+
+INSTANCE;
+
+    /**
+     * Resolves the entity type (T) from the given repository class.
+     *
+     * @param repositoryClass the repository interface
+     * @return the resolved entity class
+     * @throws NullPointerException if {@code repositoryClass} is null
+     * @throws IllegalArgumentException if the entity type cannot be resolved
+     */
+    public Class<?> resolveEntityType(Class<?> repositoryClass) {
+        Objects.requireNonNull(repositoryClass, "repositoryClass is required");
+
+        return resolveFromClass(repositoryClass)
+                .orElseThrow(() -> new IllegalArgumentException(
+                        "Cannot resolve entity type from repository: " + repositoryClass.getName()
+                ));
+    }
+
+    private Optional<Class<?>> resolveFromClass(Class<?> clazz) {
+
+        // 1. Inspect interfaces
+        for (Type type : clazz.getGenericInterfaces()) {
+            Optional<Class<?>> resolved = resolveFromType(type);
+            if (resolved.isPresent()) {
+                return resolved;
+            }
+        }
+
+        // 2. Inspect superclass (important for proxies / abstract layers)
+        Type superclass = clazz.getGenericSuperclass();
+
+        if (superclass != null) {
+            Optional<Class<?>> resolved = resolveFromType(superclass);
+            if (resolved.isPresent()) {
+                return resolved;
+            }
+
+            if (superclass instanceof Class<?> superClass) {
+                return resolveFromClass(superClass);
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    private Optional<Class<?>> resolveFromType(Type type) {
+
+        if (type instanceof ParameterizedType parameterizedType) {
+
+            Type rawType = parameterizedType.getRawType();
+
+            if (rawType instanceof Class<?> rawClass) {
+
+                // Check if it's a repository type
+                if (isRepositoryType(rawClass)) {
+                    Type entityType = parameterizedType.getActualTypeArguments()[0];
+
+                    if (entityType instanceof Class<?> entityClass) {
+                        return Optional.of(entityClass);
+                    }
+                }
+
+                // Recursive resolution (interface extends interface)
+                return resolveFromClass(rawClass);
+            }
+        }
+
+        if (type instanceof Class<?> clazz) {
+            return resolveFromClass(clazz);
+        }
+
+        return Optional.empty();
+    }
+
+    private boolean isRepositoryType(Class<?> clazz) {
+        String name = clazz.getName();
+
+        return name.equals("jakarta.data.repository.BasicRepository")
+                || name.equals("jakarta.data.repository.CrudRepository")
+                || name.equals("jakarta.data.repository.DataRepository");
+    }
+}
