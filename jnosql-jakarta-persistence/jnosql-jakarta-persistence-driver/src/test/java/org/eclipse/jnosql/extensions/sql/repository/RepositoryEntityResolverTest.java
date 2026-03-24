e5c12c1543d9706fd32cef66761173e558994871
Otavio Santana <otaviopolianasantana@gmail.com>
2026-03-24 07:51:08 +0000

    test: add unit tests for RepositoryEntityResolver entity type resolution and error handling

--- jnosql-jakarta-persistence/jnosql-jakarta-persistence-driver/src/test/java/org/eclipse/jnosql/extensions/sql/repository/RepositoryEntityResolverTest.java
+++ jnosql-jakarta-persistence/jnosql-jakarta-persistence-driver/src/test/java/org/eclipse/jnosql/extensions/sql/repository/RepositoryEntityResolverTest.java
@@ -15,8 +15,116 @@
  */
 package org.eclipse.jnosql.extensions.sql.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.assertj.core.api.SoftAssertions;
+import org.eclipse.jnosql.extensions.sql.model.Computer;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class RepositoryEntityResolverTest {
+    interface BaseBasicRepository extends jakarta.data.repository.BasicRepository<Computer, Long> {}
 
+    interface BaseCrudRepository extends jakarta.data.repository.CrudRepository<Computer, Long> {}
+
+    interface BaseDataRepository extends jakarta.data.repository.DataRepository<Computer, Long> {}
+
+    interface NoSqlRepository extends org.eclipse.jnosql.mapping.NoSQLRepository<Computer, Long> {}
+
+    interface Level1 extends BaseBasicRepository {}
+    interface Level2 extends Level1 {}
+    interface Level3 extends Level2 {}
+    interface MultiRepository extends BaseCrudRepository, BaseDataRepository {}
+    interface InvalidRepository {}
+
+
+    @Nested
+    @DisplayName("WhenResolvingEntityType")
+    class WhenResolvingEntityType {
+
+        @Test
+        @DisplayName("Should resolve entity from BasicRepository")
+        void shouldResolveFromBasicRepository() {
+
+            var result = RepositoryEntityResolver.INSTANCE.resolveEntityType(BaseBasicRepository.class);
+
+            SoftAssertions.assertSoftly(softly ->
+                    softly.assertThat(result).isEqualTo(Computer.class)
+            );
+        }
+
+        @Test
+        @DisplayName("Should resolve entity from CrudRepository")
+        void shouldResolveFromCrudRepository() {
+
+            var result = RepositoryEntityResolver.INSTANCE.resolveEntityType(BaseCrudRepository.class);
+
+            SoftAssertions.assertSoftly(softly ->
+                    softly.assertThat(result).isEqualTo(Computer.class)
+            );
+        }
+
+        @Test
+        @DisplayName("Should resolve entity from DataRepository")
+        void shouldResolveFromDataRepository() {
+
+            var result = RepositoryEntityResolver.INSTANCE.resolveEntityType(BaseDataRepository.class);
+
+            SoftAssertions.assertSoftly(softly ->
+                    softly.assertThat(result).isEqualTo(Computer.class)
+            );
+        }
+
+        @Test
+        @DisplayName("Should resolve entity from NoSQLRepository")
+        void shouldResolveFromNoSqlRepository() {
+
+            var result = RepositoryEntityResolver.INSTANCE.resolveEntityType(NoSqlRepository.class);
+
+            SoftAssertions.assertSoftly(softly ->
+                    softly.assertThat(result).isEqualTo(Computer.class)
+            );
+        }
+
+        @Test
+        @DisplayName("Should resolve entity from deep inheritance chain")
+        void shouldResolveFromDeepInheritance() {
+
+            var result = RepositoryEntityResolver.INSTANCE.resolveEntityType(Level3.class);
+
+            SoftAssertions.assertSoftly(softly ->
+                    softly.assertThat(result).isEqualTo(Computer.class)
+            );
+        }
+
+        @Test
+        @DisplayName("Should resolve entity from multiple repository inheritance")
+        void shouldResolveFromMultipleInheritance() {
+
+            var result = RepositoryEntityResolver.INSTANCE.resolveEntityType(MultiRepository.class);
+
+            SoftAssertions.assertSoftly(softly ->
+                    softly.assertThat(result).isEqualTo(Computer.class)
+            );
+        }
+
+        @Test
+        @DisplayName("Should throw exception when repository class is null")
+        void shouldThrowWhenRepositoryIsNull() {
+
+            assertThatThrownBy(() -> RepositoryEntityResolver.INSTANCE.resolveEntityType(null))
+                    .isInstanceOf(NullPointerException.class)
+                    .hasMessage("repositoryClass is required");
+        }
+
+        @Test
+        @DisplayName("Should throw exception when entity cannot be resolved")
+        void shouldThrowWhenEntityCannotBeResolved() {
+
+            assertThatThrownBy(() -> RepositoryEntityResolver.INSTANCE.resolveEntityType(InvalidRepository.class))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("Cannot resolve entity type");
+        }
+    }
 }
