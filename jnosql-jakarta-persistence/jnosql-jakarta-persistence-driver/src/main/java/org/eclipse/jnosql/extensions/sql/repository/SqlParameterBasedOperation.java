4dbac849af9d51b5aee76ae79b1088ae1a019f6d
Otavio Santana <otaviopolianasantana@gmail.com>
2026-03-24 19:17:06 +0000

    feat: add SqlParameterBasedOperation with unsupported operation for SQL extension

---
+++ jnosql-jakarta-persistence/jnosql-jakarta-persistence-driver/src/main/java/org/eclipse/jnosql/extensions/sql/repository/SqlParameterBasedOperation.java
@@ -0,0 +1,28 @@
+/*
+ *  Copyright (c) 2026 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.extensions.sql.repository;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.jnosql.mapping.metadata.repository.spi.ParameterBasedOperation;
+import org.eclipse.jnosql.mapping.metadata.repository.spi.RepositoryInvocationContext;
+
+@ApplicationScoped
+class SqlParameterBasedOperation implements ParameterBasedOperation {
+
+    @Override
+    public <T> T execute(RepositoryInvocationContext context) {
+        throw new UnsupportedOperationException("ParameterBasedOperation is not supported yet");
+    }
+}
