71a9d68cb263073819af40413edb489d0ed8fd74
Otavio Santana <otaviopolianasantana@gmail.com>
2026-03-24 19:43:46 +0000

    feat: add ComputerUpdateRepository with update and updateVoid methods for single and batch updates

---
+++ jnosql-jakarta-persistence/jnosql-jakarta-persistence-driver/src/test/java/org/eclipse/jnosql/extensions/sql/model/ComputerUpdateRepository.java
@@ -0,0 +1,45 @@
+/*
+ *  Copyright (c) 2026 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.extensions.sql.model;
+
+import jakarta.data.repository.BasicRepository;
+import jakarta.data.repository.Insert;
+import jakarta.data.repository.Repository;
+import jakarta.data.repository.Update;
+
+import java.util.List;
+
+@Repository
+public interface ComputerUpdateRepository extends BasicRepository<Computer, Long> {
+
+    @Update
+    Computer update(Computer computer);
+
+    @Update
+    Computer[] update(Computer[] computers);
+
+    @Update
+    List<Computer> update(List<Computer> computers);
+
+
+    @Update
+    void updateVoid(Computer computer);
+
+    @Update
+    void updateVoid(Computer[] computers);
+
+    @Update
+    void updateVoid(List<Computer> computers);
+}
