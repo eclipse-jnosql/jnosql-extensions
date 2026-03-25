6ae900821655a59fc85b926109de812c98908fed
Otavio Santana <otaviopolianasantana@gmail.com>
2026-03-25 19:07:28 +0000

    test: add DeleteByOperationRepositoryTest with setup for ComputerDeleteByRepository

--- jnosql-jakarta-persistence/jnosql-jakarta-persistence-driver/src/test/java/org/eclipse/jnosql/extensions/sql/repository/DeleteByOperationRepositoryTest.java
+++ jnosql-jakarta-persistence/jnosql-jakarta-persistence-driver/src/test/java/org/eclipse/jnosql/extensions/sql/repository/DeleteByOperationRepositoryTest.java
@@ -15,6 +15,7 @@
 package org.eclipse.jnosql.extensions.sql.repository;
 
 import jakarta.inject.Inject;
+import org.assertj.core.api.SoftAssertions;
 import org.eclipse.jnosql.extensions.sql.SqlTemplate;
 import org.eclipse.jnosql.extensions.sql.model.Computer;
 import org.eclipse.jnosql.extensions.sql.model.ComputerDeleteByRepository;
@@ -45,4 +46,98 @@ import static org.assertj.core.api.Assertions.assertThat;
         this.repository = producer.get(ComputerDeleteByRepository.class, template);
     }
 
+    @Nested
+    @DisplayName("WhenUsingDeleteByRepository")
+    class WhenUsingDeleteByRepository {
+
+        @Test
+        @DisplayName("Should delete entities by model")
+        void shouldDeleteByModel() {
+
+            // given
+            var c1 = repository.save(Computer.of("MacBook Pro", 2023));
+            var c2 = repository.save(Computer.of("MacBook Pro", 2022));
+            var c3 = repository.save(Computer.of("ThinkPad", 2023));
+
+            // when
+            repository.deleteByModel("MacBook Pro");
+
+            // then
+            var result = repository.findAll().toList();
+
+            SoftAssertions.assertSoftly(softly -> {
+                softly.assertThat(result).hasSize(1);
+                softly.assertThat(result.get(0).getModel()).isEqualTo("ThinkPad");
+            });
+
+            // cleanup
+            repository.deleteById(c3.getId());
+        }
+
+        @Test
+        @DisplayName("Should delete entities by release and return count")
+        void shouldDeleteByReleaseAndReturnCount() {
+
+            // given
+            var c1 = repository.save(Computer.of("MacBook Pro", 2023));
+            var c2 = repository.save(Computer.of("ThinkPad", 2023));
+            var c3 = repository.save(Computer.of("Dell XPS", 2022));
+
+            // when
+            int deleted = repository.deleteByRelease(2023);
+
+            // then
+            var result = repository.findAll().toList();
+
+            SoftAssertions.assertSoftly(softly -> {
+                softly.assertThat(deleted).isEqualTo(2);
+                softly.assertThat(result).hasSize(1);
+                softly.assertThat(result.get(0).getModel()).isEqualTo("Dell XPS");
+            });
+
+            // cleanup
+            repository.deleteById(c3.getId());
+        }
+
+        @Test
+        @DisplayName("Should not delete when model does not match")
+        void shouldNotDeleteWhenModelDoesNotMatch() {
+
+            // given
+            var c1 = repository.save(Computer.of("MacBook Pro", 2023));
+
+            // when
+            repository.deleteByModel("NonExisting");
+
+            // then
+            var result = repository.findAll().toList();
+
+            SoftAssertions.assertSoftly(softly -> {
+                softly.assertThat(result).hasSize(1);
+                softly.assertThat(result.get(0).getModel()).isEqualTo("MacBook Pro");
+            });
+
+            // cleanup
+            repository.deleteById(c1.getId());
+        }
+
+        @Test
+        @DisplayName("Should return zero when release does not match")
+        void shouldReturnZeroWhenReleaseDoesNotMatch() {
+
+            // given
+            var c1 = repository.save(Computer.of("MacBook Pro", 2023));
+
+            // when
+            int deleted = repository.deleteByRelease(1999);
+
+            // then
+            SoftAssertions.assertSoftly(softly -> {
+                softly.assertThat(deleted).isZero();
+            });
+
+            // cleanup
+            repository.deleteById(c1.getId());
+        }
+    }
 }
