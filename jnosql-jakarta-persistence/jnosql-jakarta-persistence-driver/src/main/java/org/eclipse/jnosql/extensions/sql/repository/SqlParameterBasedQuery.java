79cf67f57a0d24d1071f9e390e326810737c05ba
Otavio Santana <otaviopolianasantana@gmail.com>
2026-04-03 20:16:53 +0100

    feat: add SqlParameterBasedQuery enum as placeholder for parameter-based SQL queries

--- jnosql-jakarta-persistence/jnosql-jakarta-persistence-driver/src/main/java/org/eclipse/jnosql/extensions/sql/repository/SqlParameterBasedQuery.java
+++ jnosql-jakarta-persistence/jnosql-jakarta-persistence-driver/src/main/java/org/eclipse/jnosql/extensions/sql/repository/SqlParameterBasedQuery.java
@@ -14,5 +14,147 @@
  */
 package org.eclipse.jnosql.extensions.sql.repository;
 
+import jakarta.data.Sort;
+import jakarta.data.page.PageRequest;
+import jakarta.data.repository.By;
+import jakarta.enterprise.inject.spi.CDI;
+import org.eclipse.jnosql.communication.Condition;
+import org.eclipse.jnosql.communication.semistructured.CriteriaCondition;
+import org.eclipse.jnosql.communication.semistructured.Element;
+import org.eclipse.jnosql.communication.semistructured.SelectQuery;
+import org.eclipse.jnosql.mapping.core.Converters;
+import org.eclipse.jnosql.mapping.core.NoSQLPage;
+import org.eclipse.jnosql.mapping.core.repository.ParamValue;
+import org.eclipse.jnosql.mapping.metadata.EntityMetadata;
+import org.eclipse.jnosql.mapping.metadata.FieldMetadata;
+import org.eclipse.jnosql.mapping.semistructured.MappingQuery;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.IntFunction;
+
+import static org.eclipse.jnosql.mapping.core.util.ConverterUtil.getValue;
+
 enum SqlParameterBasedQuery {
+
+    INSTANCE;
+    private static final IntFunction<CriteriaCondition[]> TO_ARRAY = CriteriaCondition[]::new;
+
+
+    public SelectQuery toQuery(Map<String, ParamValue> params,
+                               List<Sort<?>> sorts,
+                               EntityMetadata entityMetadata) {
+        var convert = CDI.current().select(Converters.class).get();
+        List<CriteriaCondition> conditions = new ArrayList<>();
+        for (Map.Entry<String, ParamValue> entry : params.entrySet()) {
+            conditions.add(condition(convert, entityMetadata, entry));
+        }
+
+        List<Sort<?>> updateSorter = getSorts(sorts, entityMetadata);
+
+        var condition = condition(conditions);
+        var entity = entityMetadata.name();
+        return new MappingQuery(updateSorter, 0L, 0L, condition, entity, List.of());
+    }
+
+    SelectQuery toQueryNative(Map<String, Object> params,
+                                                                                     List<Sort<?>> sorts, PageRequest pageRequest,
+                                                                                     EntityMetadata entityMetadata) {
+        List<CriteriaCondition> conditions = new ArrayList<>();
+        for (Map.Entry<String, Object> entry : params.entrySet()) {
+            conditions.add(condition(entityMetadata, entry));
+        }
+
+        List<Sort<?>> updateSorter = getSorts(sorts, entityMetadata);
+
+        var condition = condition(conditions);
+        var entity = entityMetadata.name();
+        long limit = 0;
+        long skip = 0;
+        if (pageRequest != null) {
+            limit = pageRequest.size();
+            skip = NoSQLPage.skip(pageRequest);
+        }
+        return new MappingQuery(updateSorter, limit, skip, condition, entity, List.of());
+    }
+
+    private CriteriaCondition condition(List<CriteriaCondition> conditions) {
+        if (conditions.isEmpty()) {
+            return null;
+        } else if (conditions.size() == 1) {
+            return conditions.getFirst();
+        }
+        return CriteriaCondition.and(conditions.toArray(TO_ARRAY));
+    }
+
+    private CriteriaCondition condition(Converters convert, EntityMetadata entityMetadata,Map.Entry<String, ParamValue> entry) {
+        var fieldName = resolveFieldName(entityMetadata, entry.getKey());
+        var paramValue = entry.getValue();
+        var condition = paramValue.condition();
+        var value = extractConditionValue(paramValue.value(), condition, entityMetadata, entry.getKey(), convert);
+
+        return paramValue.negate() ? CriteriaCondition.of(Element.of(fieldName, value), condition).negate():
+                CriteriaCondition.of(Element.of(fieldName, value), condition);
+    }
+
+    private String resolveFieldName(EntityMetadata metadata, String key) {
+        if (By.ID.equals(key)) {
+            return metadata.id().orElseThrow().name();
+        }
+        return metadata.fieldMapping(key).map(FieldMetadata::name).orElse(key);
+    }
+
+    private Object extractConditionValue(Object rawValue, Condition condition, EntityMetadata metadata,
+                                         String fieldKey, Converters convert) {
+        boolean isCollectionParameter = rawValue instanceof Iterable<?> || rawValue != null && rawValue.getClass().isArray();
+
+        if (Condition.BETWEEN.equals(condition) || Condition.IN.equals(condition)) {
+            if (!isCollectionParameter) {
+                throw new IllegalArgumentException("The value for condition " + condition + " must be a Iterable or array, but received: " + rawValue);
+            }
+            return extractMultipleValues(rawValue, metadata, fieldKey, convert, condition);
+        }
+        if(isCollectionParameter) {
+            throw new IllegalArgumentException("The value for condition " + condition + " must be a single value, but received: " + rawValue);
+        }
+        return getValue(rawValue, metadata, fieldKey, convert);
+    }
+
+    private List<Object> extractMultipleValues(Object rawValue, EntityMetadata metadata, String fieldKey,
+                                               Converters convert, Condition condition) {
+        List<Object> values = new ArrayList<>();
+        if (rawValue instanceof Iterable<?> iterable) {
+            iterable.forEach(v -> values.add(getValue(v, metadata, fieldKey, convert)));
+        } else if (rawValue != null && rawValue.getClass().isArray()) {
+            for (int i = 0; i < Array.getLength(rawValue); i++) {
+                Object element = Array.get(rawValue, i);
+                values.add(getValue(element, metadata, fieldKey, convert));
+            }
+        }
+        if (Condition.BETWEEN.equals(condition) && values.size() != 2) {
+            throw new IllegalArgumentException("The value for condition " + condition + " must have exactly two elements, but received: " + values);
+        }
+        return values;
+    }
+
+    private CriteriaCondition condition(EntityMetadata entityMetadata, Map.Entry<String, Object> entry) {
+        var name = entityMetadata.fieldMapping(entry.getKey())
+                .map(FieldMetadata::name)
+                .orElse(entry.getKey());
+        var value = entry.getValue();
+        return CriteriaCondition.eq(name, value);
+    }
+
+    private List<Sort<?>> getSorts(List<Sort<?>> sorts, EntityMetadata entityMetadata) {
+        List<Sort<?>> updateSorter = new ArrayList<>();
+        for (Sort<?> sort : sorts) {
+            var name = entityMetadata.fieldMapping(sort.property())
+                    .map(FieldMetadata::name)
+                    .orElse(sort.property());
+            updateSorter.add(sort.isAscending() ? Sort.asc(name) : Sort.desc(name));
+        }
+        return updateSorter;
+    }
 }
