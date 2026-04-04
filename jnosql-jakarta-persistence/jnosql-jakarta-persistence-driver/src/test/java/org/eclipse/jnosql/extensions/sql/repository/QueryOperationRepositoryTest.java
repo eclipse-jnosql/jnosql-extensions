3e4c4c843f8389291ee5329c2db571a64d2d82b8
Otavio Santana <otaviopolianasantana@gmail.com>
2026-04-04 20:22:19 +0100

    test: add nested tests for ComputerQueryRepository query methods

--- jnosql-jakarta-persistence/jnosql-jakarta-persistence-driver/src/test/java/org/eclipse/jnosql/extensions/sql/repository/QueryOperationRepositoryTest.java
+++ jnosql-jakarta-persistence/jnosql-jakarta-persistence-driver/src/test/java/org/eclipse/jnosql/extensions/sql/repository/QueryOperationRepositoryTest.java
@@ -15,12 +15,15 @@
 package org.eclipse.jnosql.extensions.sql.repository;
 
 import jakarta.inject.Inject;
+import org.assertj.core.api.SoftAssertions;
 import org.eclipse.jnosql.extensions.sql.SqlTemplate;
 import org.eclipse.jnosql.extensions.sql.model.Computer;
 import org.eclipse.jnosql.extensions.sql.model.ComputerQueryRepository;
 import org.jboss.weld.junit5.EnableWeld;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 
 @EnableWeld
 @DisplayName("Query Operation Repository Tests")
@@ -40,5 +43,95 @@ class QueryOperationRepositoryTest extends AbstractTestRepository {
         template.deleteAll(Computer.class);
     }
 
+    @Nested
+    @DisplayName("WhenUsingQueryRepository")
+    class WhenUsingQueryRepository {
+
+        @Test
+        @DisplayName("Should return all computers")
+        void shouldReturnAllComputers() {
+
+            // given
+            var c1 = repository.save(Computer.of("MacBook Pro", 2023));
+            var c2 = repository.save(Computer.of("ThinkPad", 2022));
+
+            // when
+            var result = repository.allComputers();
+
+            // then
+            SoftAssertions.assertSoftly(softly -> {
+                softly.assertThat(result).hasSize(2);
+            });
+
+            // cleanup
+            repository.deleteById(c1.getId());
+            repository.deleteById(c2.getId());
+        }
+
+        @Test
+        @DisplayName("Should find computer by id")
+        void shouldFindById() {
+
+            // given
+            var computer = repository.save(Computer.of("MacBook Pro", 2023));
+
+            // when
+            var result = repository.computersBy(computer.getId());
+
+            // then
+            SoftAssertions.assertSoftly(softly -> {
+                softly.assertThat(result).hasSize(1);
+                softly.assertThat(result.getFirst().getId()).isEqualTo(computer.getId());
+            });
+
+            // cleanup
+            repository.deleteById(computer.getId());
+        }
+
+        @Test
+        @DisplayName("Should find computers by model")
+        void shouldFindByModel() {
+
+            // given
+            var c1 = repository.save(Computer.of("MacBook Pro", 2023));
+            var c2 = repository.save(Computer.of("MacBook Pro", 2022));
+            var c3 = repository.save(Computer.of("ThinkPad", 2023));
+
+            // when
+            var result = repository.computersBy("MacBook Pro");
+
+            // then
+            SoftAssertions.assertSoftly(softly -> {
+                softly.assertThat(result).hasSize(2);
+                softly.assertThat(result)
+                        .extracting(Computer::getModel)
+                        .containsOnly("MacBook Pro");
+            });
+
+            // cleanup
+            repository.deleteById(c1.getId());
+            repository.deleteById(c2.getId());
+            repository.deleteById(c3.getId());
+        }
+
+        @Test
+        @DisplayName("Should return empty when no match is found")
+        void shouldReturnEmptyWhenNoMatch() {
+
+            // given
+            var c1 = repository.save(Computer.of("MacBook Pro", 2023));
+
+            // when
+            var result = repository.computersBy("NonExisting");
+
+            // then
+            SoftAssertions.assertSoftly(softly -> {
+                softly.assertThat(result).isEmpty();
+            });
+
+            // cleanup
+            repository.deleteById(c1.getId());
+        }
+    }
 
 }
