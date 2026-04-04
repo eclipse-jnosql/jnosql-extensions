24da2b21508c33c5778a1419ff94119e7740f73d
Otavio Santana <otaviopolianasantana@gmail.com>
2026-04-04 21:19:51 +0100

    test: add integration tests for cursor pagination repository methods

--- jnosql-jakarta-persistence/jnosql-jakarta-persistence-driver/src/test/java/org/eclipse/jnosql/extensions/sql/repository/CursorPaginationOperationRepositoryTest.java
+++ jnosql-jakarta-persistence/jnosql-jakarta-persistence-driver/src/test/java/org/eclipse/jnosql/extensions/sql/repository/CursorPaginationOperationRepositoryTest.java
@@ -14,6 +14,7 @@
  */
 package org.eclipse.jnosql.extensions.sql.repository;
 
+import jakarta.data.page.PageRequest;
 import jakarta.inject.Inject;
 import org.eclipse.jnosql.extensions.sql.SqlTemplate;
 import org.eclipse.jnosql.extensions.sql.model.Computer;
@@ -44,6 +45,123 @@ class CursorPaginationOperationRepositoryTest extends AbstractTestRepository {
     @BeforeEach
     void setUp() {
         this.repository = producer.get(ComputerCursorPaginationRepository.class, template);
+    }
+
+    @Nested
+    @DisplayName("WhenUsingCursorPaginationRepository")
+    class WhenUsingCursorPaginationRepository {
+
+        @BeforeEach
+        void setUp() {
+            template.deleteAll(Computer.class);
+        }
+
+        @Test
+        @DisplayName("Should return first page ordered by id")
+        void shouldReturnFirstPageOrderedById() {
+
+            // given
+            var c1 = repository.save(Computer.of("MacBook Pro", 2023));
+            var c2 = repository.save(Computer.of("MacBook Pro", 2022));
+            var c3 = repository.save(Computer.of("MacBook Pro", 2021));
+
+            // when
+            var page = repository.findByModel(
+                    "MacBook Pro",
+                    PageRequest.ofSize(2)
+            );
+
+            // then
+            assertThat(page.content()).hasSize(2);
+            assertThat(page.content())
+                    .extracting(Computer::getId)
+                    .isSorted();
+
+            // cleanup
+            repository.deleteById(c1.getId());
+            repository.deleteById(c2.getId());
+            repository.deleteById(c3.getId());
+        }
+
+        @Test
+        @DisplayName("Should navigate to next cursor page")
+        void shouldNavigateToNextPage() {
+
+            // given
+            var c1 = repository.save(Computer.of("MacBook Pro", 2023));
+            var c2 = repository.save(Computer.of("MacBook Pro", 2022));
+            var c3 = repository.save(Computer.of("MacBook Pro", 2021));
+
+            var firstPage = repository.findByModel(
+                    "MacBook Pro",
+                    PageRequest.ofSize(2)
+            );
+
+            var nextCursor = firstPage.nextPageRequest();
+
+            // when
+            var secondPage = repository.findByModel(
+                    "MacBook Pro",
+                    nextCursor
+            );
+
+            // then
+            assertThat(secondPage.content()).hasSize(1);
+
+            // cleanup
+            repository.deleteById(c1.getId());
+            repository.deleteById(c2.getId());
+            repository.deleteById(c3.getId());
+        }
+
+        @Test
+        @DisplayName("Should return empty when no match is found")
+        void shouldReturnEmptyWhenNoMatch() {
+
+            // given
+            repository.save(Computer.of("ThinkPad", 2023));
+
+            // when
+            var page = repository.findByModel(
+                    "MacBook Pro",
+                    PageRequest.ofSize(2)
+            );
+
+            // then
+            assertThat(page.content()).isEmpty();
+        }
+
+        @Test
+        @DisplayName("Should behave consistently between Find and annotation-based methods")
+        void shouldReturnSameResultsForBothMethods() {
+
+            // given
+            var c1 = repository.save(Computer.of("MacBook Pro", 2023));
+            var c2 = repository.save(Computer.of("MacBook Pro", 2022));
+
+            var page1 = repository.findByModel(
+                    "MacBook Pro",
+                    PageRequest.ofSize(10)
+            );
+
+            var page2 = repository.model(
+                    "MacBook Pro",
+                    PageRequest.ofSize(10)
+            );
+
+            // then
+            assertThat(page1.content())
+                    .extracting(Computer::getId)
+                    .containsExactlyElementsOf(
+                            page2.content().stream()
+                                    .map(Computer::getId)
+                                    .toList()
+                    );
+
+            // cleanup
+            repository.deleteById(c1.getId());
+            repository.deleteById(c2.getId());
+        }
     }
 
 }
