0235c0b806eb6d4a25df632d534053af64e4bfde
Otavio Santana <otaviopolianasantana@gmail.com>
2026-04-07 20:28:11 +0100

    feat: inject SqlTemplate into NoopRepository and throw UnsupportedOperationException for all methods

--- jnosql-jakarta-persistence/jnosql-jakarta-persistence-driver/src/main/java/org/eclipse/jnosql/extensions/sql/repository/NoopRepository.java
+++ jnosql-jakarta-persistence/jnosql-jakarta-persistence-driver/src/main/java/org/eclipse/jnosql/extensions/sql/repository/NoopRepository.java
@@ -18,6 +18,7 @@ package org.eclipse.jnosql.extensions.sql.repository;
 import jakarta.data.Order;
 import jakarta.data.page.Page;
 import jakarta.data.page.PageRequest;
+import org.eclipse.jnosql.extensions.sql.SqlTemplate;
 import org.eclipse.jnosql.mapping.NoSQLRepository;
 
 import java.util.List;
@@ -25,88 +26,95 @@ import java.util.Optional;
 import java.util.stream.Stream;
 
 final class NoopRepository<T, K>  implements NoSQLRepository<T, K> {
+
+    private final SqlTemplate sqlTemplate;
+
+    NoopRepository(SqlTemplate sqlTemplate) {
+        this.sqlTemplate = sqlTemplate;
+    }
+
     @Override
     public void deleteAll() {
-
+        throw new UnsupportedOperationException("The operation is not supported when entity is not defined");
     }
 
     @Override
     public long countBy() {
-        return 0;
+        throw new UnsupportedOperationException("The operation is not supported when entity is not defined");
     }
 
     @Override
     public boolean existsById(K id) {
-        return false;
+        throw new UnsupportedOperationException("The operation is not supported when entity is not defined");
     }
 
     @Override
     public Stream<T> findByIdIn(Iterable<K> ids) {
-        return Stream.empty();
+        throw new UnsupportedOperationException("The operation is not supported when entity is not defined");
     }
 
     @Override
     public void deleteByIdIn(Iterable<K> ids) {
-
+        throw new UnsupportedOperationException("The operation is not supported when entity is not defined");
     }
 
     @Override
     public <S extends T> S insert(S entity) {
-        return null;
+        throw new UnsupportedOperationException("The operation is not supported when entity is not defined");
     }
 
     @Override
     public <S extends T> List<S> insertAll(List<S> entities) {
-        return List.of();
+        throw new UnsupportedOperationException("The operation is not supported when entity is not defined");
     }
 
     @Override
     public <S extends T> S update(S entity) {
-        return null;
+        throw new UnsupportedOperationException("The operation is not supported when entity is not defined");
     }
 
     @Override
     public <S extends T> List<S> updateAll(List<S> entities) {
-        return List.of();
+        throw new UnsupportedOperationException("The operation is not supported when entity is not defined");
     }
 
     @Override
     public <S extends T> S save(S entity) {
-        return null;
+        throw new UnsupportedOperationException("The operation is not supported when entity is not defined");
     }
 
     @Override
     public <S extends T> List<S> saveAll(List<S> entities) {
-        return List.of();
+        throw new UnsupportedOperationException("The operation is not supported when entity is not defined");
     }
 
     @Override
     public Optional<T> findById(K id) {
-        return Optional.empty();
+        throw new UnsupportedOperationException("The operation is not supported when entity is not defined");
     }
 
     @Override
     public Stream<T> findAll() {
-        return Stream.empty();
+        throw new UnsupportedOperationException("The operation is not supported when entity is not defined");
     }
 
     @Override
     public Page<T> findAll(PageRequest pageRequest, Order<T> sortBy) {
-        return null;
+        throw new UnsupportedOperationException("The operation is not supported when entity is not defined");
     }
 
     @Override
     public void deleteById(K id) {
-
+        throw new UnsupportedOperationException("The operation is not supported when entity is not defined");
     }
 
     @Override
     public void delete(T entity) {
-
+        throw new UnsupportedOperationException("The operation is not supported when entity is not defined");
     }
 
     @Override
     public void deleteAll(List<? extends T> entities) {
-
+        throw new UnsupportedOperationException("The operation is not supported when entity is not defined");
     }
 }
