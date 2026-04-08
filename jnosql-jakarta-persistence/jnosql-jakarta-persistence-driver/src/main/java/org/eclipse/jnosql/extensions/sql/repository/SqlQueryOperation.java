9509413b27c2e6eaf7ee43bf497d72329db0f7dc
Otavio Santana <otaviopolianasantana@gmail.com>
2026-04-08 06:39:22 +0100

    refactor: implement initial logic for execute method in SqlQueryOperation

--- jnosql-jakarta-persistence/jnosql-jakarta-persistence-driver/src/main/java/org/eclipse/jnosql/extensions/sql/repository/SqlQueryOperation.java
+++ jnosql-jakarta-persistence/jnosql-jakarta-persistence-driver/src/main/java/org/eclipse/jnosql/extensions/sql/repository/SqlQueryOperation.java
@@ -15,14 +15,31 @@
 package org.eclipse.jnosql.extensions.sql.repository;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.jnosql.communication.query.data.QueryType;
+import org.eclipse.jnosql.extensions.sql.SqlTemplate;
+import org.eclipse.jnosql.mapping.core.repository.DynamicReturn;
 import org.eclipse.jnosql.mapping.metadata.repository.spi.QueryOperation;
 import org.eclipse.jnosql.mapping.metadata.repository.spi.RepositoryInvocationContext;
+
+import java.util.logging.Logger;
 
 @ApplicationScoped
 class SqlQueryOperation implements QueryOperation {
 
+    private static final Logger LOGGER = Logger.getLogger(SqlQueryOperation.class.getName());
+
     @Override
     public <T> T execute(RepositoryInvocationContext context) {
-        throw new UnsupportedOperationException("QueryOperation is not supported by SQL extension");
+        var entityMetadata = context.entityMetadata();
+        var method = context.method();
+        var params = context.parameters();
+        var template = (SqlTemplate) context.template();
+        Class<?> type = entityMetadata.type();
+        var pageRequest = DynamicReturn.findPageRequest(params);
+        var queryValue = method.query().orElseThrow();
+        var queryType = QueryType.parse(queryValue);
+        var returnType = method.returnType().orElseThrow();
+        LOGGER.finest("Query: " + queryValue + " with type: " + queryType + " and return type: " + returnType);
+        queryType.checkValidReturn(returnType, queryValue);
     }
 }
