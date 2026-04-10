7f52e42bb4da9589c3cff87f12404e139a371469
Otavio Santana <otaviopolianasantana@gmail.com>
2026-04-10 19:42:35 +0100

    refactor: implement prepare method in SqlDeleteQueryParser and add query parsing logic

--- jnosql-jakarta-persistence/jnosql-jakarta-persistence-driver/src/main/java/org/eclipse/jnosql/extensions/sql/SqlDeleteQueryParser.java
+++ jnosql-jakarta-persistence/jnosql-jakarta-persistence-driver/src/main/java/org/eclipse/jnosql/extensions/sql/SqlDeleteQueryParser.java
@@ -14,5 +14,38 @@
  */
 package org.eclipse.jnosql.extensions.sql;
 
+import org.eclipse.jnosql.communication.Params;
+import org.eclipse.jnosql.communication.query.data.DeleteProvider;
+import org.eclipse.jnosql.communication.semistructured.CommunicationObserverParser;
+import org.eclipse.jnosql.communication.semistructured.Conditions;
+import org.eclipse.jnosql.communication.semistructured.CriteriaCondition;
+import org.eclipse.jnosql.communication.semistructured.DefaultDeleteQuery;
+import org.eclipse.jnosql.communication.semistructured.DeleteQuery;
+
+import java.util.List;
+
 final class SqlDeleteQueryParser {
+
+    SqlPreparedStatement prepare(String query, SqlTemplate template) {
+        Params params = Params.newParams();
+        DeleteQuery deleteQuery = getQuery(query, params);
+        return SqlPreparedStatement.delete(deleteQuery, params, query, template);
+    }
+
+    private DeleteQuery getQuery(String query, Params params) {
+        org.eclipse.jnosql.communication.query.DeleteQuery deleteQuery = DeleteProvider.INSTANCE.apply(query);
+
+        return getQuery(params, deleteQuery);
+    }
+
+    private DeleteQuery getQuery(Params params, org.eclipse.jnosql.communication.query.DeleteQuery deleteQuery) {
+        String columnFamily = CommunicationObserverParser.EMPTY.fireEntity(deleteQuery.entity());
+        List<String> columns = deleteQuery.fields().stream()
+                .map(f -> CommunicationObserverParser.EMPTY.fireSelectField(columnFamily, f))   .toList();
+        CriteriaCondition condition = deleteQuery.where().map(c -> Conditions.getCondition(c, params, CommunicationObserverParser.EMPTY, columnFamily))
+                .orElse(null);
+
+        return new DefaultDeleteQuery(columnFamily, condition, columns);
+    }
+
 }
