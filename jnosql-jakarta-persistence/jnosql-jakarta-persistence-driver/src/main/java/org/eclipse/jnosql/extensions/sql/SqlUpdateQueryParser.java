6d978dc1be018383d5b23cf4ad21e397111540a1
Otavio Santana <otaviopolianasantana@gmail.com>
2026-04-10 19:49:17 +0100

    refactor: implement prepare method in SqlUpdateQueryParser and add update query parsing logic

--- jnosql-jakarta-persistence/jnosql-jakarta-persistence-driver/src/main/java/org/eclipse/jnosql/extensions/sql/SqlUpdateQueryParser.java
+++ jnosql-jakarta-persistence/jnosql-jakarta-persistence-driver/src/main/java/org/eclipse/jnosql/extensions/sql/SqlUpdateQueryParser.java
@@ -14,5 +14,48 @@
  */
 package org.eclipse.jnosql.extensions.sql;
 
+import org.eclipse.jnosql.communication.Params;
+import org.eclipse.jnosql.communication.query.UpdateItem;
+import org.eclipse.jnosql.communication.query.data.UpdateProvider;
+import org.eclipse.jnosql.communication.semistructured.CommunicationObserverParser;
+import org.eclipse.jnosql.communication.semistructured.Conditions;
+import org.eclipse.jnosql.communication.semistructured.CriteriaCondition;
+import org.eclipse.jnosql.communication.semistructured.DefaultUpdateQuery;
+import org.eclipse.jnosql.communication.semistructured.Element;
+import org.eclipse.jnosql.communication.semistructured.UpdateQuery;
+import org.eclipse.jnosql.communication.semistructured.Values;
+
+import java.util.ArrayList;
+import java.util.List;
+
 final class SqlUpdateQueryParser {
+
+
+    SqlPreparedStatement prepare(String query, SqlTemplate template) {
+
+        Params params = Params.newParams();
+        var updateQuery = getQuery(query, params);
+        return SqlPreparedStatement.update(updateQuery, params, query, template);
+    }
+
+    private UpdateQuery getQuery(String query, Params params) {
+        var updateQuery = UpdateProvider.INSTANCE.apply(query);
+        return getQuery(params, updateQuery);
+    }
+
+    private UpdateQuery getQuery(Params params, org.eclipse.jnosql.communication.query.UpdateQuery updateQuery) {
+        var entity = CommunicationObserverParser.EMPTY.fireEntity(updateQuery.entity());
+
+        List<Element> set = new ArrayList<>();
+        for (UpdateItem updateItem : updateQuery.set()) {
+            var field = CommunicationObserverParser.EMPTY.fireSelectField(entity, updateItem.name());
+            var value = Values.get(updateItem.value(), params);
+            set.add(Element.of(field, value));
+        }
+        CriteriaCondition condition = updateQuery.where().map(c -> Conditions.getCondition(c, params, CommunicationObserverParser.EMPTY, entity))
+                .orElse(null);
+
+        return new DefaultUpdateQuery(entity, set, condition);
+    }
+
 }
