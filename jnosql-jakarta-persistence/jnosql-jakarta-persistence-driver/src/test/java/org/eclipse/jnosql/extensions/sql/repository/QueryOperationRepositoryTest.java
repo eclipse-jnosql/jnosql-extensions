21b8f05065639a2b893a9092674a3f688a222081
Otavio Santana <otaviopolianasantana@gmail.com>
2026-04-11 06:30:46 +0100

    test: add initial QueryOperationRepositoryTest setup with dependency injection

---
+++ jnosql-jakarta-persistence/jnosql-jakarta-persistence-driver/src/test/java/org/eclipse/jnosql/extensions/sql/repository/QueryOperationRepositoryTest.java
@@ -0,0 +1,51 @@
+/*
+ *  Copyright (c) 2026 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.extensions.sql.repository;
+
+import jakarta.inject.Inject;
+import org.eclipse.jnosql.extensions.sql.SqlTemplate;
+import org.eclipse.jnosql.extensions.sql.model.Computer;
+import org.eclipse.jnosql.extensions.sql.model.ComputerQueryRepository;
+import org.eclipse.jnosql.extensions.sql.model.ComputerSaveRepository;
+import org.jboss.weld.junit5.EnableWeld;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@EnableWeld
+@DisplayName("Query Operation Repository Tests")
+class QueryOperationRepositoryTest extends AbstractTestRepository {
+
+    @Inject
+    private SqlTemplate template;
+
+    @Inject
+    private SqlRepositoryProducer producer;
+
+    private ComputerQueryRepository repository;
+
+    @BeforeEach
+    void setUp() {
+        this.repository = producer.get(ComputerQueryRepository.class, template);
+    }
+
+
+
+}
