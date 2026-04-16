2b9bfb2a2ca4b9a69075d53c9797f98f5894aed0
Otavio Santana <otaviopolianasantana@gmail.com>
2026-04-16 07:49:38 +0100

    chore: delegate toPredicate to PredicateConverter and remove redundant logic in QueryConverterSupport

--- jnosql-jakarta-persistence/jnosql-jakarta-persistence-driver/src/main/java/org/eclipse/jnosql/extensions/sql/QueryConverterSupport.java
+++ jnosql-jakarta-persistence/jnosql-jakarta-persistence-driver/src/main/java/org/eclipse/jnosql/extensions/sql/QueryConverterSupport.java
@@ -21,10 +21,7 @@ import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Path;
 import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
-import org.eclipse.jnosql.communication.TypeReference;
-import org.eclipse.jnosql.communication.Value;
 import org.eclipse.jnosql.communication.semistructured.CriteriaCondition;
-import org.eclipse.jnosql.communication.semistructured.Element;
 
 import java.util.List;
 
@@ -43,123 +40,12 @@ abstract class QueryConverterSupport {
         PREDICATE_CONVERTER.applyCondition(criteriaCondition, criteriaBuilder, root, criteriaQuery);
     }
 
-    @SuppressWarnings({"unchecked", "rawtypes"})
     protected Predicate toPredicate(CriteriaCondition condition,
                                  CriteriaBuilder criteriaBuilder,
                                  Root<?> root) {
-
-        Element element = condition.element();
-
-        String property = element.name();
-        Object rawValue = element.get();
-        Path<?> path = path(root, property, rawValue);
-        return switch (condition.condition()) {
-
-            case EQUALS ->
-                    criteriaBuilder.equal(path, rawValue);
-
-            case GREATER_THAN ->
-                    criteriaBuilder.greaterThan(
-                            path.as(Comparable.class),
-                            (Comparable) rawValue);
-
-            case GREATER_EQUALS_THAN ->
-                    criteriaBuilder.greaterThanOrEqualTo(
-                            path.as(Comparable.class),
-                            (Comparable) rawValue);
-
-            case LESSER_THAN ->
-                    criteriaBuilder.lessThan(
-                            path.as(Comparable.class),
-                            (Comparable) rawValue);
-
-            case LESSER_EQUALS_THAN ->
-                    criteriaBuilder.lessThanOrEqualTo(
-                            path.as(Comparable.class),
-                            (Comparable) rawValue);
-
-            case LIKE ->
-                    criteriaBuilder.like(path.as(String.class), rawValue.toString());
-
-            case CONTAINS ->
-                    criteriaBuilder.like(path.as(String.class), "%" + rawValue + "%");
-
-            case STARTS_WITH ->
-                    criteriaBuilder.like(path.as(String.class), rawValue + "%");
-
-            case ENDS_WITH ->
-                    criteriaBuilder.like(path.as(String.class), "%" + rawValue);
-
-            case IN -> {
-                CriteriaBuilder.In<Object> in = criteriaBuilder.in(path);
-                ((Iterable<?>) rawValue).forEach(v -> in.value(value(v)));
-                yield in;
-            }
-
-            case BETWEEN -> {
-                var values = (List<?>) rawValue;
-                yield criteriaBuilder.between(
-                        path.as(Comparable.class),
-                        (Comparable) value(values.get(0)),
-                        (Comparable) value(values.get(1)));
-            }
-
-            case AND -> {
-                List<CriteriaCondition> conditions =
-                        condition.element().value().get(new TypeReference<>() {});
-
-                List<Predicate> andPredicates = conditions.stream()
-                        .map(c -> toPredicate(c, criteriaBuilder, root))
-                        .toList();
-
-                yield criteriaBuilder.and(andPredicates.toArray(new Predicate[0]));
-            }
-
-            case OR -> {
-                List<CriteriaCondition> conditions =
-                        condition.element().value().get(new TypeReference<>() {});
-
-                List<Predicate> orPredicates = conditions.stream()
-                        .map(c -> toPredicate(c, criteriaBuilder, root))
-                        .toList();
-
-                yield criteriaBuilder.or(orPredicates.toArray(new Predicate[0]));
-            }
-
-            case NOT -> {
-                var inner = element.get(CriteriaCondition.class);
-                yield criteriaBuilder.not(
-                        toPredicate(inner, criteriaBuilder, root)
-                );
-            }
-
-            case IGNORE_CASE -> {
-                var inner = element.get(CriteriaCondition.class);
-                var innerElement = inner.element();
-                var innerProperty = innerElement.name();
-                Object innerValue = innerElement.get();
-                Path<?> innerPath = path(root, innerProperty, innerValue);
-                yield criteriaBuilder.equal(
-                        criteriaBuilder.lower(innerPath.as(String.class)),
-                        innerValue.toString().toLowerCase()
-                );
-            }
-        };
+        return PREDICATE_CONVERTER.toPredicate(condition, criteriaBuilder, root);
     }
 
-    private static Object value(Object value) {
-        if(value instanceof Value paramValue) {
-            return paramValue.get();
-        }
-        return value;
-    }
-
-    private Path<?> path(Root<?> root, String property, Object value) {
-        if(value instanceof CriteriaCondition) {
-            return null;
-        }
-        return resolvePath(root, property);
-    }
 
     static Path<?> resolvePath(Path<?> root, String property) {
 
@@ -201,9 +87,5 @@ abstract class QueryConverterSupport {
                 .findFirst()
                 .orElseThrow(() ->
                         new IllegalArgumentException("Entity not found: " + name));
-    }
-
-    private boolean isStringPath(Path<?> path) {
-        return path != null && String.class.isAssignableFrom(path.getJavaType());
     }
 }
