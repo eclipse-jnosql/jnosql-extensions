9e0cb1b399c4474213c52da0ab025c733c425b8d
Maximillian Arruda <dearrudam@gmail.com>
2026-04-21 22:13:16 -0300

    feat: implement Book Management REST API
    Added REST endpoints for managing Book entities, enabling
    CRUD operations through the JNoSQL Spring Boot integration
    with MongoDB. This includes the creation of necessary
    DTOs, services, and controllers for handling requests.
    
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

---
+++ jnosql-spring-boot/jnosql-spring-boot-test-projects/jnosql-spring-boot-starter-mongodb-test/src/main/java/org/eclipse/jnosql/spring/boot/starter/book/controller/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+/*
+ *  Copyright (c) 2026 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Maximillian Arruda
+ */
+package org.eclipse.jnosql.spring.boot.starter.book.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Void> handleValidationException(MethodArgumentNotValidException ex) {
+        return ResponseEntity.badRequest().build();
+    }
+}
