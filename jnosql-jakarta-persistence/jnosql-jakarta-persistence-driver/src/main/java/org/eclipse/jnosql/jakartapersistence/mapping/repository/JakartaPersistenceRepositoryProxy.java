393888afa3bbd3902b340b3686fd326c3479a522
Otavio Santana <otaviopolianasantana@gmail.com>
2026-05-30 13:15:29 +0100

    chore: remove unused Function import from JakartaPersistenceRepositoryProxy

--- jnosql-jakarta-persistence/jnosql-jakarta-persistence-driver/src/main/java/org/eclipse/jnosql/jakartapersistence/mapping/repository/JakartaPersistenceRepositoryProxy.java
+++ jnosql-jakarta-persistence/jnosql-jakarta-persistence-driver/src/main/java/org/eclipse/jnosql/jakartapersistence/mapping/repository/JakartaPersistenceRepositoryProxy.java
@@ -53,7 +53,6 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.function.BiFunction;
-import java.util.function.Function;
 import java.util.function.LongSupplier;
 import java.util.logging.Logger;
 import java.util.stream.Stream;
