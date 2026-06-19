e9681e82eb08471aec9a20cea99cb1b4846553a0
Otavio Santana <otaviopolianasantana@gmail.com>
2026-06-19 19:17:27 +0100

    feat: collect valid auto-apply AttributeConverter entries in processor

--- jnosql-lite/mapping-lite-processor/src/main/java/org/eclipse/jnosql/lite/mapping/converter/AutoApplyConverterProcessor.java
+++ jnosql-lite/mapping-lite-processor/src/main/java/org/eclipse/jnosql/lite/mapping/converter/AutoApplyConverterProcessor.java
@@ -14,6 +14,7 @@
  */
 package org.eclipse.jnosql.lite.mapping.converter;
 
+import jakarta.nosql.AttributeConverter;
 import jakarta.nosql.Converter;
 
 import javax.annotation.processing.AbstractProcessor;
@@ -21,7 +22,10 @@ import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.DeclaredType;
+import javax.lang.model.type.TypeMirror;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -31,28 +35,13 @@ import java.util.logging.Logger;
 @SupportedSourceVersion(SourceVersion.RELEASE_21)
 public class AutoApplyConverterProcessor extends AbstractProcessor {
 
-    private static final Logger LOGGER = Logger.getLogger(AutoApplyConverterProcessor.class.getName());
+
+    private static final Logger LOGGER =
+            Logger.getLogger(AutoApplyConverterProcessor.class.getName());
 
     @Override
-    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        roundEnv.getElementsAnnotatedWith(jakarta.nosql.Converter.class)
-                .stream()
-                .filter(TypeElement.class::isInstance)
-                .map(TypeElement.class::cast)
-                .filter(this::isAutoApply)
-                .forEach(converter -> LOGGER.fine(() ->
-                        "Auto-apply converter found: " + converter.getQualifiedName()));
-
-
-        return false;
-    }
-
-    private boolean isAutoApply(TypeElement converter) {
-        var annotation = converter.getAnnotation(jakarta.nosql.Converter.class);
-        return annotation != null && annotation.autoApply();
-    }
-
-    private AutoApplyConverterModel buildModel(RoundEnvironment roundEnv) {
+    public boolean process(Set<? extends TypeElement> annotations,
+                           RoundEnvironment roundEnv) {
 
         List<ConverterEntryType> converterTypes = new ArrayList<>();
         List<ConverterEntryInstance> converterInstances = new ArrayList<>();
@@ -62,27 +51,61 @@ public class AutoApplyConverterProcessor extends AbstractProcessor {
                 .filter(TypeElement.class::isInstance)
                 .map(TypeElement.class::cast)
                 .filter(this::isAutoApply)
-                .filter(this::implementsAttributeConverter)
                 .forEach(converter -> {
 
-                    String attributeType = attributeType(converter);
+                    DeclaredType attributeConverter =
+                            attributeConverter(converter);
+
+                    if (attributeConverter == null) {
+                        LOGGER.warning(() -> "Ignoring converter " + converter.getQualifiedName() + " because it does not implement "
+                                + AttributeConverter.class.getName());
+                        return;
+                    }
+
+                    String attributeType = attributeType(attributeConverter);
                     String converterType = converter.getQualifiedName().toString();
 
-                    converterTypes.add(
-                            new ConverterEntryType(
-                                    attributeType + ".class",
-                                    converterType + ".class"
-                            )
-                    );
-
-                    converterInstances.add(
-                            new ConverterEntryInstance(
-                                    attributeType + ".class",
-                                    "new " + converterType + "()"
-                            )
-                    );
+                    converterTypes.add(new ConverterEntryType(attributeType + ".class", converterType + ".class"));
+                    converterInstances.add(new ConverterEntryInstance(attributeType + ".class", "new " + converterType + "()"));
                 });
 
-        return new AutoApplyConverterModel(converterTypes, converterInstances);
+        AutoApplyConverterModel model = new AutoApplyConverterModel(converterTypes, converterInstances);
+
+        LOGGER.fine(() -> "Found " + converterTypes.size() + " auto-apply converters");
+        // TODO generate source file using model
+        return false;
     }
+
+    private boolean isAutoApply(TypeElement converter) {
+        Converter annotation = converter.getAnnotation(Converter.class);
+        return annotation != null && annotation.autoApply();
+    }
+
+    private DeclaredType attributeConverter(TypeElement converter) {
+
+        for (TypeMirror mirror : converter.getInterfaces()) {
+
+            if (!(mirror instanceof DeclaredType declaredType)) {
+                continue;
+            }
+
+            Element element = declaredType.asElement();
+
+            if (element instanceof TypeElement typeElement
+                    && typeElement.getQualifiedName()
+                    .contentEquals(AttributeConverter.class.getName())) {
+                return declaredType;
+            }
+        }
+
+        return null;
+    }
+
+    private String attributeType(DeclaredType converter) {
+        return processingEnv.getTypeUtils()
+                .erasure(converter.getTypeArguments().get(0))
+                .toString();
+    }
+
+
 }
