69d660465676494836ce422eb6cdc702bc0d879f
Otavio Santana <otaviopolianasantana@gmail.com>
2026-06-19 18:54:23 +0100

    feat: add AutoApplyConverterModel class

--- jnosql-lite/mapping-lite-processor/src/main/java/org/eclipse/jnosql/lite/mapping/converter/AutoApplyConverterModel.java
+++ jnosql-lite/mapping-lite-processor/src/main/java/org/eclipse/jnosql/lite/mapping/converter/AutoApplyConverterModel.java
@@ -1,4 +1,15 @@
 package org.eclipse.jnosql.lite.mapping.converter;
 
+import java.time.LocalDateTime;
+import java.time.Year;
+
 public class AutoApplyConverterModel {
+
+    public LocalDateTime getNow() {
+        return LocalDateTime.now();
+    }
+
+    public String getCurrentYear(){
+        return Year.now().toString();
+    }
 }
