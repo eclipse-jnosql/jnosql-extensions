719498cccc9c507cf1e4ea55e27ffb888ee6685e
Otavio Santana <otaviopolianasantana@gmail.com>
2026-06-19 21:58:47 +0100

    fix: remove unused imports from auto apply converter processor

--- jnosql-lite/mapping-lite-processor/src/main/java/org/eclipse/jnosql/lite/mapping/converter/AutoApplyConverterProcessor.java
+++ jnosql-lite/mapping-lite-processor/src/main/java/org/eclipse/jnosql/lite/mapping/converter/AutoApplyConverterProcessor.java
@@ -16,14 +16,12 @@ package org.eclipse.jnosql.lite.mapping.converter;
 
 import com.github.mustachejava.DefaultMustacheFactory;
 import com.github.mustachejava.Mustache;
-import com.github.mustachejava.MustacheFactory;
 import jakarta.nosql.AttributeConverter;
 import jakarta.nosql.Converter;
 import jakarta.nosql.MappingException;
 import org.eclipse.jnosql.lite.mapping.metadata.AutoApplyConverterMetadata;
 
 import javax.annotation.processing.AbstractProcessor;
-import javax.annotation.processing.Filer;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
@@ -32,15 +30,11 @@ import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.DeclaredType;
 import javax.lang.model.type.TypeMirror;
-import javax.tools.JavaFileObject;
 import javax.tools.StandardLocation;
 import java.io.IOException;
 import java.io.Writer;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-import java.util.function.Consumer;
 import java.util.logging.Logger;
 
 @SupportedAnnotationTypes("jakarta.nosql.Converter")
