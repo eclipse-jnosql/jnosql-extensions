ebab0894f3114ca52df95bb2cac043e458e3af65
Otavio Santana <otaviopolianasantana@gmail.com>
2026-06-19 18:52:16 +0100

    feat: configure AutoApplyConverterProcessor as an annotation processor

--- jnosql-lite/mapping-lite-processor/src/main/java/org/eclipse/jnosql/lite/mapping/converter/AutoApplyConverterProcessor.java
+++ jnosql-lite/mapping-lite-processor/src/main/java/org/eclipse/jnosql/lite/mapping/converter/AutoApplyConverterProcessor.java
@@ -15,11 +15,19 @@
 package org.eclipse.jnosql.lite.mapping.converter;
 
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.TypeElement;
+import java.util.Set;
 
 @SupportedAnnotationTypes("jakarta.nosql.Converter")
 @SupportedSourceVersion(SourceVersion.RELEASE_21)
 public class AutoApplyConverterProcessor extends AbstractProcessor {
+    
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        return false;
+    }
 }
