1dc98bad15d3d04d04e5be8759b5157fd9d0213e
Otavio Santana <otaviopolianasantana@gmail.com>
2026-06-20 12:58:33 +0100

    chore: add license header to DatabaseTest class

--- jnosql-lite/mapping-lite-core-test/src/test/java/org/eclipse/jnosql/lite/mapping/entities/DatabaseTest.java
+++ jnosql-lite/mapping-lite-core-test/src/test/java/org/eclipse/jnosql/lite/mapping/entities/DatabaseTest.java
@@ -14,7 +14,60 @@
  */
 package org.eclipse.jnosql.lite.mapping.entities;
 
+import org.eclipse.jnosql.lite.mapping.entities.converters.Database;
+import org.eclipse.jnosql.lite.mapping.entities.record.Guest;
+import org.eclipse.jnosql.lite.mapping.metadata.LiteEntitiesMetadata;
+import org.eclipse.jnosql.mapping.metadata.EntitiesMetadata;
+import org.eclipse.jnosql.mapping.metadata.EntityMetadata;
+import org.eclipse.jnosql.mapping.metadata.FieldMetadata;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
 public class DatabaseTest {
 
+    private EntitiesMetadata mappings;
 
+    private EntityMetadata entityMetadata;
+
+    @BeforeEach
+    public void setUp() {
+        this.mappings = new LiteEntitiesMetadata();
+        this.entityMetadata = this.mappings.get(Database.class);
+    }
+
+    @Test
+    void shouldGetName() {
+        Assertions.assertEquals("Database", entityMetadata.name());
+    }
+
+    @Test
+    void shouldGetSimpleName() {
+        Assertions.assertEquals(Database.class.getSimpleName(), entityMetadata.simpleName());
+    }
+
+    @Test
+    void shouldGetClassName() {
+        Assertions.assertEquals(Database.class.getName(), entityMetadata.className());
+    }
+
+    @Test
+    void shouldGetClassInstance() {
+        Assertions.assertEquals(Database.class, entityMetadata.type());
+    }
+
+    @Test
+    void shouldGetId() {
+        Optional<FieldMetadata> id = this.entityMetadata.id();
+        org.assertj.core.api.Assertions.assertThat(id).isPresent();
+    }
+
+    @Test
+    void shouldHaveAutoApplyConverter() {
+        Optional<FieldMetadata> id = this.entityMetadata.fieldMapping("id");
+        org.assertj.core.api.Assertions.assertThat(id).isPresent();
+        org.assertj.core.api.Assertions.assertThat(id.get().converter()).isPresent();
+    }
 }
