235164286890a6b75389f8155a467f2845aea56f
Otavio Santana <otaviopolianasantana@gmail.com>
2026-08-06 08:27:50 +0100

    chore: add lifecycle event handler tests for typed car observer events

--- jnosql-lite/mapping-lite-core-test/src/test/java/org/eclipse/jnosql/lite/mapping/metadata/LiteLifecycleEventHandlerTest.java
+++ jnosql-lite/mapping-lite-core-test/src/test/java/org/eclipse/jnosql/lite/mapping/metadata/LiteLifecycleEventHandlerTest.java
@@ -15,10 +15,16 @@
 package org.eclipse.jnosql.lite.mapping.metadata;
 
 import jakarta.inject.Inject;
+import org.eclipse.jnosql.lite.mapping.entities.Car;
 import org.eclipse.jnosql.mapping.repository.LifecycleEventHandler;
 import org.jboss.weld.junit5.auto.AddBeanClasses;
 import org.jboss.weld.junit5.auto.EnableAutoWeld;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @EnableAutoWeld
 @AddBeanClasses({
@@ -31,6 +37,146 @@ class LiteLifecycleEventHandlerTest {
     @Inject
     private LifecycleEventHandler listener;
 
+    @Inject
+    private CarLifecycleEventObserver observer;
 
+    private Car car;
 
+    @BeforeEach
+    void setUp() {
+        this.car = new Car("roadster", "Model 3");
+        this.observer.reset();
+    }
+
+    @Nested
+    @DisplayName("When firing insert events")
+    class WhenInsert {
+
+        @Test
+        @DisplayName("Should deliver a typed pre-insert event to the entity observer")
+        void shouldFirePreInsertEvent() {
+            // when
+            listener.preInsert(car);
+
+            // then
+            assertThat(observer.events())
+                    .containsExactly(
+                            new ObservedLifecycleEvent(
+                                    ObservedLifecycleEventType.PRE_INSERT,
+                                    car));
+        }
+
+        @Test
+        @DisplayName("Should deliver a typed post-insert event to the entity observer")
+        void shouldFirePostInsertEvent() {
+            // when
+            listener.postInsert(car);
+
+            // then
+            assertThat(observer.events())
+                    .containsExactly(
+                            new ObservedLifecycleEvent(
+                                    ObservedLifecycleEventType.POST_INSERT,
+                                    car));
+        }
+    }
+
+    @Nested
+    @DisplayName("When firing update events")
+    class WhenUpdate {
+
+        @Test
+        @DisplayName("Should deliver a typed pre-update event to the entity observer")
+        void shouldFirePreUpdateEvent() {
+            // when
+            listener.preUpdate(car);
+
+            // then
+            assertThat(observer.events())
+                    .containsExactly(
+                            new ObservedLifecycleEvent(
+                                    ObservedLifecycleEventType.PRE_UPDATE,
+                                    car));
+        }
+
+        @Test
+        @DisplayName("Should deliver a typed post-update event to the entity observer")
+        void shouldFirePostUpdateEvent() {
+            // when
+            listener.postUpdate(car);
+
+            // then
+            assertThat(observer.events())
+                    .containsExactly(
+                            new ObservedLifecycleEvent(
+                                    ObservedLifecycleEventType.POST_UPDATE,
+                                    car));
+        }
+    }
+
+    @Nested
+    @DisplayName("When firing upsert events")
+    class WhenUpsert {
+
+        @Test
+        @DisplayName("Should deliver a typed pre-upsert event to the entity observer")
+        void shouldFirePreUpsertEvent() {
+            // when
+            listener.preUpsert(car);
+
+            // then
+            assertThat(observer.events())
+                    .containsExactly(
+                            new ObservedLifecycleEvent(
+                                    ObservedLifecycleEventType.PRE_UPSERT,
+                                    car));
+        }
+
+        @Test
+        @DisplayName("Should deliver a typed post-upsert event to the entity observer")
+        void shouldFirePostUpsertEvent() {
+            // when
+            listener.postUpsert(car);
+
+            // then
+            assertThat(observer.events())
+                    .containsExactly(
+                            new ObservedLifecycleEvent(
+                                    ObservedLifecycleEventType.POST_UPSERT,
+                                    car));
+        }
+    }
+
+    @Nested
+    @DisplayName("When firing delete events")
+    class WhenDelete {
+
+        @Test
+        @DisplayName("Should deliver a typed pre-delete event to the entity observer")
+        void shouldFirePreDeleteEvent() {
+            // when
+            listener.preDelete(car);
+
+            // then
+            assertThat(observer.events())
+                    .containsExactly(
+                            new ObservedLifecycleEvent(
+                                    ObservedLifecycleEventType.PRE_DELETE,
+                                    car));
+        }
+
+        @Test
+        @DisplayName("Should deliver a typed post-delete event to the entity observer")
+        void shouldFirePostDeleteEvent() {
+            // when
+            listener.postDelete(car);
+
+            // then
+            assertThat(observer.events())
+                    .containsExactly(
+                            new ObservedLifecycleEvent(
+                                    ObservedLifecycleEventType.POST_DELETE,
+                                    car));
+        }
+    }
 }
