589f0b1a65358556fb93c44d205aaed934c86bb0
Otavio Santana <otaviopolianasantana@gmail.com>
2026-09-01 11:04:48 +0100

    chore: update Jakarta NoSQL and Testcontainers versions and align TCK template supplier naming

--- jnosql-nosql-tck-runner/src/test/java/org/eclipse/jnosql/tck/DocumentDatabase.java
+++ jnosql-nosql-tck-runner/src/test/java/org/eclipse/jnosql/tck/DocumentDatabase.java
@@ -14,19 +14,8 @@
  */
 package org.eclipse.jnosql.tck;
 
-import com.mongodb.ConnectionString;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import org.eclipse.jnosql.communication.Settings;
-import org.eclipse.jnosql.databases.mongodb.communication.MongoDBDocumentConfiguration;
-import org.eclipse.jnosql.databases.mongodb.communication.MongoDBDocumentConfigurations;
-import org.eclipse.jnosql.databases.mongodb.communication.MongoDBDocumentManager;
-import org.eclipse.jnosql.databases.mongodb.communication.MongoDBDocumentManagerFactory;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public enum DocumentDatabase {
 
